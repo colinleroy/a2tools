40874ad2ceef6a6d4a6f268d6b9b6219b38a8063
Colin Leroy-Mira <colin@colino.net>
2022-12-10 14:12:05 +0100

    simple_serial starting to look like something

--- mcmillen/src/lib/simple_serial.h
+++ mcmillen/src/lib/simple_serial.h
@@ -1,8 +1,18 @@
 #include <serial.h>
+#include <stdlib.h>
 #include <conio.h>
 
 int simple_serial_open(int slot, int baudrate);
 
-int simple_serial_close();
+int simple_serial_close(void);
 
-char *simple_serial_gets();
+void simple_serial_set_timeout(int timeout);
+
+char simple_serial_getc(void);
+int simple_serial_getc_with_timeout(void);
+
+char *simple_serial_gets(char *out, size_t size);
+char *simple_serial_gets_with_timeout(char *out, size_t size);
+
+size_t simple_serial_read(char *ptr, size_t size, size_t nmemb);
+size_t simple_serial_read_with_timeout(char *ptr, size_t size, size_t nmemb);
--- mcmillen/src/recvtxt/recvtxt.c
+++ mcmillen/src/recvtxt/recvtxt.c
@@ -4,21 +4,37 @@
 #include <string.h>
 #include <dbg.h>
 
-#define HEAD 0
-#define N_KNOTS 10
+#define BUF_SIZE 255
 
 
 int main(void) {
-  int t,total_lines = 0;
+  int t, total_lines = 0, tm;
+  char *buf = malloc(BUF_SIZE);
+  char *large_buf = malloc(4096);
 
   simple_serial_open(2, SER_BAUD_9600);
 
-  for (t = 0; t < 10; t++) {
-    char *str = simple_serial_gets();
-    printf("Read '%s'\n", str);
-    free(str);
+  if (simple_serial_gets(buf, BUF_SIZE) != NULL) {
+    printf("Read '%s'\n", buf);
   }
+
+  tm = atoi(buf);
+  printf("setting read timeout to %d\n", tm);
+  simple_serial_set_timeout(tm);
+
+  printf("Reading buffer without timeout\n");
+  t = simple_serial_read(large_buf, sizeof(char), 4096);
+
+  printf("Read buffer of %d bytes\n", t);
+
+  printf("Reading buffer with timeout\n");
+  t = simple_serial_read_with_timeout(large_buf, sizeof(char), 4096);
+
+  printf("Read buffer of %d bytes\n", t);
+
+  free(buf);
+  free(large_buf);
+
   simple_serial_close();
-  cgetc();
   exit(0);
 }
