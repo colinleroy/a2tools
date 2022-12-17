7aea75ade72e531e44fbd594111f1539b2285090
Colin Leroy-Mira <colin@colino.net>
2022-12-18 00:45:48 +0100

    Cleaner algo

--- a2tools/src/aoc/day17/day17.c
+++ a2tools/src/aoc/day17/day17.c
@@ -18,48 +18,36 @@ static void read_file(FILE *fp);
 
 #define DATASET "IN17E"
 
-char shape_masks[5][4][4] = {
-  {"    ",
-   "    ",
-   "    ",
-   "####"
+int shape_masks[5][4][4] = {
+  {{0,0,0,0},
+   {0,0,0,0},
+   {0,0,0,0},
+   {1,1,1,1}
  },
-  {"    ",
-   " #  ",
-   "### ",
-   " #  "
+  {{0,0,0,0},
+   {0,1,0,0},
+   {1,1,1,0},
+   {0,1,0,0}
  },
-  {"    ",
-   "  # ",
-   "  # ",
-   "### "
+  {{0,0,0,0},
+   {0,0,1,0},
+   {0,0,1,0},
+   {1,1,1,0}
  },
-  {"#   ",
-   "#   ",
-   "#   ",
-   "#   "
+  {{1,0,0,0},
+   {1,0,0,0},
+   {1,0,0,0},
+   {1,0,0,0}
  },
-  {"    ",
-   "    ",
-   "##  ",
-   "##  "
+  {{0,0,0,0},
+   {0,0,0,0},
+   {1,1,0,0},
+   {1,1,0,0}
  },
 };
-int shape_widths[5]     = {4, 3, 3, 1, 2};
-int shape_heights[5][4] = {
- {1,1,1,1},
- {2,3,2,0},
- {1,1,3,0},
- {4,0,0,0},
- {2,2,0,0},
-};
-int shape_bottom_offsets[5][4] = {
- {0,0,0,0},
- {1,0,1,-1},
- {0,0,0,-1},
- {0,-1,-1,-1},
- {0,0,-1,-1},
-};
+
+int shape_widths[5]  = {4, 3, 3, 1, 2};
+int shape_heights[5] = {4, 3, 3, 4, 2};
 
 int main(void) {
   FILE *fp;
@@ -82,146 +70,177 @@ int main(void) {
 #define WIND_LEFT 0
 
 
-static int min_floor_height(int *floor_height) {
-  int i;
-  int h = 0;
-  for (i = 0; i < WALLS_WIDTH; i++) {
-    if (floor_height[i] < h) {
-      h = floor_height[i];
-    }
-  }
-  return h;
-}
-
-static int max_floor_height(int *floor_height) {
-  int i;
-  int h = 0;
-  for (i = 0; i < WALLS_WIDTH; i++) {
-    if (floor_height[i] > h) {
-      h = floor_height[i];
-    }
-  }
-  return h;
-}
 #define VIZ
 #ifdef VIZ
 static char *blank_line = NULL;
 static char *cutoff_line = NULL;
 static char *bottom_line = NULL;
-static void viz(char *msg, int iterations, int cur_shape, int cur_shape_left, int cur_shape_bottom, int cur_wind, int *floor_height) {
+static void viz(char *msg, int iterations, bool_array *table, int cur_shape, int cur_shape_left, int cur_shape_bottom, int cur_wind, int floor_height) {
   int cur_shape_top = cur_shape_bottom + 4;
-  int max_fh = max_floor_height(floor_height);
-  int min_fh = min_floor_height(floor_height);
+  int max_fh = floor_height;
   int line = max_fh + 10;
   int i, c;
+  int x, y;
 
 #ifdef __CC65__
   gotoxy(1, 1);
 #endif
-  if (iterations != NUM_ITERATIONS) {
-    if (iterations > 10 && iterations % 10)
-      return;
-    else if (iterations > 100 && iterations % 100)
-      return;
+  // if (iterations != NUM_ITERATIONS) {
+  //   if (iterations > 10 && iterations % 10)
+  //     return;
+  //   else if (iterations > 100 && iterations % 100)
+  //     return;
+  // }
+  for (x = 0; x < WALLS_WIDTH; x++) {
+    printf("%d", x);
   }
-  printf("|%s| (%d) Iter. %d\n", blank_line, max_fh + 10, iterations);
-  printf("|%s| (%d) Height %d\n", blank_line, max_fh + 9, max_fh);
-  for (i = max_fh + 8; i > min_fh; i--) {
-    if (cur_shape_top < i) {
-      printf("|%s| (%d)\n", blank_line, i);
-    } else if (cur_shape_top >= i && cur_shape_bottom < i) {
-      printf("|");
-      for (c = 0; c < cur_shape_left; c++) {
-        printf("%c", i > floor_height[c] ? ' ':'@');
+  printf("\n");
+  for (y = max_fh + 7; y >= max_fh - 19 && y >= 0; y--) {
+    for (x = 0; x < WALLS_WIDTH; x++) {
+      if (y <= cur_shape_top - 1 && y > cur_shape_bottom - 1
+       && x >= cur_shape_left && x < cur_shape_left + shape_widths[cur_shape]
+       && shape_masks[cur_shape][cur_shape_top - y - 1][x - cur_shape_left]) {
+        printf("#");
+      } else if (bool_array_get(table, x, y)) {
+        printf("@");
+      } else {
+        printf(".");
       }
-      for (c = 0; c < shape_widths[cur_shape]; c++) {
-        printf("%c", shape_masks[cur_shape][cur_shape_top - i][c] == '#' ? '#':' ');
-      }
-      for (c = cur_shape_left + shape_widths[cur_shape]; c < WALLS_WIDTH; c++) {
-        printf("%c", i > floor_height[c] ? ' ':'@');
-      }
-      printf("| (%d) %c\n", i, cur_wind == WIND_LEFT ? '<':'>');
-    } else if (i <= cur_shape_bottom && i > max_fh) {
-      printf("|%s| (%d)\n", blank_line, i);
-    } else if (i <= max_fh && i >= max_fh - 5) {
-      printf("|");
-      for (c = 0; c < WALLS_WIDTH; c++) {
-        if (i > floor_height[c])
-          printf(" ");
-        else
-          printf("@");
-      }
-      printf("| (%d)\n", i);
-    } else if (i < max_fh - 5) {
-      printf("|%s| (%d-%d)\n", cutoff_line, i, min_fh + 1);
-      break;
     }
+    printf(" (%d)\n", y);
   }
-  printf("+%s+ (0) - %s\n", bottom_line, msg);
+  printf("+%s+ (0) - %s (%d,%d)\n", bottom_line, msg, cur_shape_left, cur_shape_bottom);
   getc(stdin);
 }
 #else
-#define viz(msg, iterations, cur_shape, cur_shape_left, cur_shape_bottom, cur_wind, floor_height) do {} while (0)
+#define viz(msg, iterations, table, cur_shape, cur_shape_left, cur_shape_bottom, cur_wind) do {} while (0)
 #endif
 
-static int intersect(int cur_shape, int cur_shape_left, int cur_shape_bottom, int **floor_height) {
-  int i, intersected = 0;
-  for (i = cur_shape_left; i < cur_shape_left + shape_widths[cur_shape]; i++) {
-    if (shape_bottom_offsets[cur_shape][i - cur_shape_left] != -1) {
-      if (cur_shape_bottom + shape_bottom_offsets[cur_shape][i - cur_shape_left] == (*floor_height)[i]) {
-        intersected = 1;
-        break;
+static int can_move_left(bool_array *table, int shape, int x, int y) {
+  int i, d_x;
+  if (x == 0) {
+    return 0;
+  }
+
+  /* check each row */
+  for (i = 0; i < shape_heights[shape]; i++) {
+    /* shift to first block of shape at that line */
+    int shape_left_start;
+    for (shape_left_start = 0, d_x = x; 
+         shape_left_start < shape_widths[shape] && d_x < WALLS_WIDTH
+         && shape_masks[shape][3 - i][shape_left_start] == 0;
+         shape_left_start++, d_x++);
+    if (bool_array_get(table, d_x - 1, y) != 0) {
+      return 0;
+    }
+  }
+  return 1;
+}
+
+static int can_move_right(bool_array *table, int shape,int x, int y) {
+  int i, d_x;
+  if (x + shape_widths[shape] == WALLS_WIDTH) {
+    return 0;
+  }
+
+
+  /* check each row */
+  for (i = 0; i < shape_heights[shape]; i++) {
+    /* shift to first block of shape at that line */
+    int shape_right_start = 4 - shape_widths[shape];
+    int d_x = x + shape_widths[shape];
+
+    while (shape_right_start >= 0 && shape_masks[shape][3 - i][shape_right_start] == 0) {
+      shape_right_start--;
+      d_x--;
+    }
+
+    if (bool_array_get(table, d_x + 1, y) != 0) {
+      return 0;
+    }
+  }
+  return 1;
+}
+
+static int can_move_down(bool_array *table, int shape, int x, int y) {
+  int i;
+
+  /* check each column */
+  for (i = 0; i < shape_widths[shape]; i++) {
+    /* shift to first block of shape at that line */
+    int shape_bottom_start = 3;
+    int d_y = y;
+    
+    while (shape_bottom_start >= 0 && shape_masks[shape][shape_bottom_start][i] == 0) {
+      shape_bottom_start--;
+      d_y++;
+    }
+    
+    if (bool_array_get(table, x + i, d_y - 1) != 0) {
+      printf("can't move down! %d,%d is blocked\n", x, d_y-1);
+      return 0;
+    }
+  }
+  return 1;
+}
+
+static void land(bool_array *table, int shape, int x, int y, int *floor_height) {
+  int s_x, s_y, t_x, t_y;
+
+  for (s_x = 0, t_x = x; s_x < shape_widths[shape]; s_x++, t_x++) {
+    for (s_y = 3, t_y = y; s_y >= 0; s_y--, t_y++) {
+      if (shape_masks[shape][s_y][s_x]) {
+        bool_array_set(table, t_x, t_y, 1);
+        if (t_y > *floor_height) {
+          *floor_height = t_y;
+        }
       }
     }
   }
-  if (intersected) {
-    for (i = cur_shape_left; i < cur_shape_left + shape_widths[cur_shape]; i++) {
-      (*floor_height)[i] = cur_shape_bottom + shape_heights[cur_shape][i - cur_shape_left];
-    }
-  }
-  return intersected;
 }
 
-static void simulate_falls(bool_array *wind, int wind_pattern_length) {
+static void simulate_falls(bool_array *wind, bool_array *table, int wind_pattern_length) {
   int cur_shape = 0;
   int iterations = 0;
-  int *floor_height = malloc(WALLS_WIDTH * sizeof(int));
+  int floor_height = 0;
   int wind_count = 0;
   char cur_wind;
   int cur_shape_bottom, cur_shape_left;
-
-  memset(floor_height, 0, WALLS_WIDTH * sizeof(int));
+  int must_land;
 
   do {
-    cur_shape_bottom = 3 + max_floor_height(floor_height);
+    cur_shape_bottom = 4 + floor_height;
     cur_shape_left = 2;
+    must_land = 0;
+    viz("start round", 
+          iterations, table, cur_shape, cur_shape_left, cur_shape_bottom, cur_wind, floor_height);
 
     while (1) {
       cur_wind = (bool_array_get(wind, wind_count, 0)) == 0;
       /* get pushed */
-      if (cur_wind == WIND_RIGHT 
-       && cur_shape_left + shape_widths[cur_shape] < WALLS_WIDTH
-       && floor_height[cur_shape_left + shape_widths[cur_shape]] < cur_shape_bottom + 1) {
+      if (cur_wind == WIND_RIGHT && can_move_right(table, cur_shape, cur_shape_left, cur_shape_bottom)) {
         cur_shape_left++;
-      } else if (cur_wind == WIND_LEFT 
-              && cur_shape_left > 0
-              && floor_height[cur_shape_left - 1] < cur_shape_bottom + 1)
+      } else if (cur_wind == WIND_LEFT && can_move_left(table, cur_shape, cur_shape_left, cur_shape_bottom))
         cur_shape_left--;
 
       viz(cur_wind == WIND_LEFT ? "left" : "right", 
-            iterations, cur_shape, cur_shape_left, cur_shape_bottom, cur_wind, floor_height);
+            iterations, table, cur_shape, cur_shape_left, cur_shape_bottom, cur_wind, floor_height);
 
       /* change wind for next time */
       wind_count = (wind_count + 1) % wind_pattern_length;
 
       /* get down */
-      if (!intersect(cur_shape, cur_shape_left, cur_shape_bottom, &floor_height))
+      if (can_move_down(table, cur_shape, cur_shape_left, cur_shape_bottom)) {
         cur_shape_bottom--;
-      else
+      } else {
+        land(table, cur_shape, cur_shape_left, cur_shape_bottom, &floor_height);
+        must_land = 0;
         break;
+      }
+        
 
       viz("down", 
-            iterations, cur_shape, cur_shape_left, cur_shape_bottom, cur_wind, floor_height);
+            iterations, table, cur_shape, cur_shape_left, cur_shape_bottom, cur_wind, floor_height);
     }
 
     iterations++;
@@ -229,14 +248,13 @@ static void simulate_falls(bool_array *wind, int wind_pattern_length) {
   } while (iterations < NUM_ITERATIONS);
   
   viz("done!", 
-        iterations, cur_shape, cur_shape_left, cur_shape_bottom, cur_wind, floor_height);
+        iterations, table, cur_shape, cur_shape_left, cur_shape_bottom, cur_wind, floor_height);
 
-  free(floor_height);
 }
 
 static void read_file(FILE *fp) {
   char *buf = malloc(BUFSIZE);
-  bool_array *wind;
+  bool_array *wind, *table;
   char *c;
   int i = 0;
   int wind_pattern_length = 0;
@@ -256,6 +274,8 @@ static void read_file(FILE *fp) {
   }
   free(buf);
 
+  table = bool_array_alloc(7, 5000);
+
   blank_line = malloc((1 + WALLS_WIDTH) * sizeof(char));
   cutoff_line = malloc((1 + WALLS_WIDTH) * sizeof(char));
   bottom_line = malloc((1 + WALLS_WIDTH) * sizeof(char));
@@ -263,11 +283,12 @@ static void read_file(FILE *fp) {
     blank_line[i] = ' ';
     cutoff_line[i] = '.';
     bottom_line[i] = '-';
+    bool_array_set(table, i, 0, 1);
   }
   blank_line[i] = '\0';
   cutoff_line[i] = '\0';
   bottom_line[i] = '\0';
 
-  simulate_falls(wind, wind_pattern_length);
+  simulate_falls(wind, table, wind_pattern_length);
   bool_array_free(wind);
 }
