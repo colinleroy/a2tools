58e2688a2c978beb167ab7f795aa8baf62ad8d9d
Colin Leroy-Mira <colin@colino.net>
2022-12-17 18:12:05 +0100

    Update to use clean BFS

--- a2tools/src/aoc/day12/day12.c
+++ a2tools/src/aoc/day12/day12.c
@@ -3,19 +3,20 @@
 #include <string.h>
 #include <errno.h>
 #include "day12.h"
-#include "slist.h"
+#include "bfs.h"
 
 // Djikstra implementation
 
 #define DATASET "IN12"
 #define BUFSIZE 255
-#define DEBUG
+
+/* Used to flatten bidimensional array to unidimensional
+ * for BFS */
+#define SINGLE_DIM(x,y,y_len) (((x) * (y_len)) + (y))
 
 char buf[BUFSIZE];
 
 char **nodes = NULL;
-char **visited = NULL;
-int **distances = NULL;
 
 static void free_all(void);
 
@@ -27,10 +28,10 @@ int i, j;
 
 static void read_file(FILE *fp);
 
-static slist *build_neighbors_list(char n, int x, int y);
+static int build_neighbors_array(char n, int x, int y, short **neighbors_array);
 
 static void find_closest_node(int *closest_x, int *closest_y);
-static void calculate_path_lengths();
+static short *calculate_path_lengths();
 
 #ifdef DEBUG
 static void dump_maps(void) {
@@ -42,26 +43,13 @@ static void dump_maps(void) {
     }
     printf("\n");
   }
-  printf("Visited map:\n");
-  for (i = 0; i < max_y; i++) {
-    for (j = 0; j < max_x; j++) {
-      printf("%c",visited[i][j] == 0 ? '0':'1');
-    }
-    printf("\n");
-  }
-  printf("Distances map:\n");
-  for (i = 0; i < max_y; i++) {
-    for (j = 0; j < max_x; j++) {
-      printf("%d ",distances[i][j]);
-    }
-    printf("\n");
-  }
 }
 #endif
 
 int main(void) {
   FILE *fp;
   int closest_a = -1;
+  short *bfs_dists = NULL;
 
 #ifdef PRODOS_T_TXT
   _filetype = PRODOS_T_TXT;
@@ -80,110 +68,57 @@ int main(void) {
   dump_maps();
 #endif
 
-  calculate_path_lengths();
+  bfs_dists = calculate_path_lengths();
 
-  printf("\nPart1: Shortest path to %d,%d : %d\n", end_x, end_y, distances[end_y][end_x]);
+  printf("\nPart1: Shortest path to %d,%d : %d\n", end_x, end_y, bfs_dists[SINGLE_DIM(end_x, end_y, max_y)]);
 
   for (i = 0; i < max_y; i++) {
     for (j = 0; j < max_x; j++) {
-        if (nodes[i][j] == 'a' && distances[i][j] >= 0) {
-          if (closest_a < 0 || distances[i][j] < closest_a) {
-            closest_a = distances[i][j];
-            printf("Part2: Shortest path to an 'a' is now to %d,%d : %d\n", j, i, distances[i][j]);
+        if (nodes[i][j] == 'a' && bfs_dists[SINGLE_DIM(j, i, max_y)] >= 0) {
+          if (closest_a < 0 || bfs_dists[SINGLE_DIM(j, i, max_y)] < closest_a) {
+            closest_a = bfs_dists[SINGLE_DIM(j, i, max_y)];
           }
         }
     }
   }
+  printf("Part2: Shortest path to an 'a' is %d\n", closest_a);
   free_all();
+  free(bfs_dists);
 
   exit (0);
 }
 
-static void find_closest_node(int *closest_x, int *closest_y) {
-  int min_distance = -1;
+static short *calculate_path_lengths(void ) {
+  short *bfs_dists = NULL;
+  bfs *b = bfs_new();
   int x, y;
-  *closest_x = -1;
-  *closest_y = -1;
+  printf("adding %d nodes(for map of %dx%d)\n", max_x*max_y, max_x, max_y);
+  bfs_add_nodes(b, max_x * max_y);
 
   for (x = 0; x < max_x; x++) {
     for (y = 0; y < max_y; y++) {
-      if (visited[y][x]) {
-        continue;
-      }
-      if (distances[y][x] < 0) {
-        continue;
-      }
-      if (distances[y][x] <= min_distance || min_distance == -1) {
-        min_distance = distances[y][x];
-        *closest_x = x;
-        *closest_y = y;
-      }
+      short *neighbors = NULL;
+      int num_neighbors = build_neighbors_array(nodes[y][x], x, y, &neighbors);
+      bfs_add_paths(b, SINGLE_DIM(x, y, max_y), neighbors, num_neighbors);
+      free(neighbors);
     }
   }
-}
+  bfs_dists = bfs_compute_shortest_paths(b, SINGLE_DIM(start_x, start_y, max_y));
+  bfs_free(b);
 
-static void calculate_path_lengths(void ) {
-  int visited_count = 0;
-  distances[start_y][start_x] = 0;
-
-  printf("\n\nSTARTING.\n");
-
-  while (1) {
-    int cur_x = -1, cur_y = -1;
-    slist *neighbors, *w;
-
-    find_closest_node(&cur_x, &cur_y);
-
-    if (cur_x < 0 && cur_y < 0) {
-      return;
-    }
-
-    visited[cur_y][cur_x] = 1;
-    visited_count ++;
-
-    printf("Visiting node (%d,%d) (%d / %d visited)...\n", cur_x, cur_y, visited_count, max_x*max_y);
-
-    neighbors = build_neighbors_list(nodes[cur_y][cur_x], cur_x, cur_y);
-    for(w = neighbors; w; w = w->next) {
-      int *neighbor = (int *)w->data;
-      int neighbor_y = neighbor[0];
-      int neighbor_x = neighbor[1];
-
-      if (!visited[neighbor_y][neighbor_x] && distances[cur_y][cur_x] > -1) {
-        int cur_len = distances[cur_y][cur_x];
-        int neighbor_len = distances[neighbor_y][neighbor_x];
-
-        if (cur_len + 1 < neighbor_len || neighbor_len < 0) {
-          distances[neighbor_y][neighbor_x] = cur_len + 1;
-#ifdef DEBUG
-          printf("%s shortest_path to (%d,%d) is now %d\n",
-                 neighbor_len < 0 ? "New" : "Updated",
-                 neighbor_x, neighbor_y, distances[neighbor_y][neighbor_x]);
-#endif
-        }
-      }
-      free(neighbor);
-    }
-    slist_free(neighbors);
-  }
+  return bfs_dists;
 }
 
 static void free_all() {
   int i;
   for (i = 0; i < max_y; i++) {
     free(nodes[i]);
-    free(visited[i]);
-    free(distances[i]);
   }
   free(nodes);
-  free(visited);
-  free(distances);
 }
 
 static void read_file(FILE *fp) {
   char *node_line = NULL;
-  char *visited_line = NULL;
-  int *distances_line = NULL;
   int i;
   while (NULL != fgets(buf, sizeof(buf), fp)) {
     if (max_x == 0) {
@@ -194,43 +129,21 @@ static void read_file(FILE *fp) {
       }
     }
     buf[max_x] = '\0';
-
+//HERE
     nodes = realloc(nodes, (1 + max_y)*sizeof(char *));
-    visited = realloc(visited, (1 + max_y)*sizeof(char *));
-    distances = realloc(distances, (1 + max_y)*sizeof(int *));
 
     if (nodes == NULL) {
       printf("Couldn't realloc nodes\n");
       exit(1);
     }
-    if (visited == NULL) {
-      printf("Couldn't realloc visited\n");
-      exit(1);
-    }
-    if (distances == NULL) {
-      printf("Couldn't realloc distances\n");
-      exit(1);
-    }
 
     node_line = strdup(buf);
-    visited_line = malloc( (1 + max_x)*sizeof(char));
-    distances_line = malloc( (1 + max_x)*sizeof(int));
     if (node_line == NULL) {
       printf("Couldn't allocate node_line (%d)\n", 1 + max_x);
       exit(1);
     }
-    if (visited_line == NULL) {
-      printf("Couldn't allocate visited_line (%d)\n", 1 + max_x);
-      exit(1);
-    }
-    if (distances_line == NULL) {
-      printf("Couldn't allocate distances_line (%d)\n", 1 + max_x);
-      exit(1);
-    }
 
     nodes[max_y] = node_line;
-    visited[max_y] = visited_line;
-    distances[max_y] = distances_line;
     for (i = 0; i < max_x; i++) {
       if (node_line[i] == 'S') {
         node_line[i] = 'a';
@@ -241,45 +154,39 @@ static void read_file(FILE *fp) {
         start_x = i;
         start_y = max_y;
       }
-
-      visited_line[i] = 0;
-      distances_line[i] = -1;
     }
     max_y++;
   }
 }
 
-static slist *build_neighbors_list(char n, int x, int y) {
-  slist *neighbors = NULL;
-
+static int build_neighbors_array(char n, int x, int y, short **neighbors_array) {
+  int num_neighbors = 0;
+  short *neighbors = NULL;
   /* consider all directions */
   if (x > 0 && (nodes[y][x-1] >= n - 1)) {
-    int *coords = malloc(2*sizeof(int));
-    coords[0] = y;
-    coords[1] = x-1;
-    neighbors = slist_prepend(neighbors, coords);
+    neighbors = realloc(neighbors, (num_neighbors + 1) * sizeof(short));
+    neighbors[num_neighbors] = SINGLE_DIM(x-1, y, max_y);
+    num_neighbors++;
   }
 
   if (x < max_x - 1 && (nodes[y][x+1] >= n - 1)){
-    int *coords = malloc(2*sizeof(int));
-    coords[0] = y;
-    coords[1] = x+1;
-    neighbors = slist_prepend(neighbors, coords);
+    neighbors = realloc(neighbors, (num_neighbors + 1) * sizeof(short));
+    neighbors[num_neighbors] = SINGLE_DIM(x+1, y, max_y);
+    num_neighbors++;
   }
 
   if (y > 0 && (nodes[y-1][x] >= n - 1)){
-    int *coords = malloc(2*sizeof(int));
-    coords[0] = y-1;
-    coords[1] = x;
-    neighbors = slist_prepend(neighbors, coords);
+    neighbors = realloc(neighbors, (num_neighbors + 1) * sizeof(short));
+    neighbors[num_neighbors] = SINGLE_DIM(x, y-1, max_y);
+    num_neighbors++;
   }
 
   if (y < max_y - 1 && (nodes[y+1][x] >= n - 1)){
-    int *coords = malloc(2*sizeof(int));
-    coords[0] = y+1;
-    coords[1] = x;
-    neighbors = slist_prepend(neighbors, coords);
+    neighbors = realloc(neighbors, (num_neighbors + 1) * sizeof(short));
+    neighbors[num_neighbors] = SINGLE_DIM(x, y+1, max_y);
+    num_neighbors++;
   }
+  *neighbors_array = neighbors;
 
-  return neighbors;
+  return num_neighbors;
 }
