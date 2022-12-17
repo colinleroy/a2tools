66084a9db8565a5c5bb31e567a9f434a26d773da
Colin Leroy-Mira <colin@colino.net>
2022-12-17 16:16:10 +0100

    Little viz on the //c

--- a2tools/src/aoc/day16/day16.c
+++ a2tools/src/aoc/day16/day16.c
@@ -5,6 +5,7 @@
 #include <unistd.h>
 #ifdef __CC65__
 #include <apple2.h>
+#include <conio.h>
 #endif
 #include "bfs.h"
 #include "extended_string.h"
@@ -54,7 +55,6 @@ static int find_optimal_flow(short start_valve, short time, short **bfs_dists, s
 #endif
   int optimal_flow = 0, i;
   int time_rem;
-  int start_valve_in_targets = -1;
   short cur_valve;
 
 #ifdef DEBUG
@@ -67,14 +67,18 @@ static int find_optimal_flow(short start_valve, short time, short **bfs_dists, s
   enabled_targets[start_valve] = 0;
 
   for (i = 0; i < num_targets; i++) {
-    if (!enabled_targets[targets[i]]) {
+    cur_valve = targets[i];
+    if (!enabled_targets[cur_valve]) {
       continue;
     }
-    cur_valve = targets[i];
     /* time to travel + time to open */
     time_rem = time - bfs_dists[start_valve][cur_valve] - 1;
     if (time_rem > 0) {
       int path_flow = valve_flow[cur_valve] * time_rem;
+#ifdef __CC65__
+      gotoxy(depth, depth);
+      printf("%s (%d/%d): %d",valve_name[start_valve], i, num_targets, path_flow);
+#endif
       path_flow += find_optimal_flow(cur_valve, time_rem, bfs_dists, targets, num_targets, enabled_targets, depth + 1);
 #ifdef DEBUG
       printf("%sopen valve %s (%d * %d = %d) + \n", prefix, valve_name[i], valve_flow[i], time_rem, valve_flow[i] * time_rem);
@@ -86,6 +90,10 @@ static int find_optimal_flow(short start_valve, short time, short **bfs_dists, s
     }
   }
   enabled_targets[start_valve] = 1;
+#ifdef __CC65__
+  gotoxy(depth, depth);
+  printf("                   ");
+#endif
 
 #ifdef DEBUG
   free(prefix);
@@ -104,16 +112,24 @@ static void read_file(FILE *fp) {
   short **bfs_dists = NULL;
   short *enabled_targets;
 
+#ifdef __CC65__
+  clrscr();
+#endif
+
   while (fgets(buf, BUFSIZE-1, fp) != NULL) {
     char *name = strchr(buf, ' ') + 1;
     char *flow_rate = strchr(buf, '=') + 1;
     char *paths_str = strstr(buf, "to valve") + strlen("to valve");
     char **paths = NULL;
-    int i, num_paths;
 
     *strchr(name, ' ') ='\0';
     *strchr(flow_rate, ';') = '\0';
     paths_str = strchr(paths_str, ' ') +1;
+
+#ifdef __CC65__
+    gotoxy(1,1);
+    printf("Reading valve %s...", name);
+#endif
 
     valve_name             = realloc(valve_name, (count + 1) * sizeof(char *));
     valve_destinations_str = realloc(valve_destinations_str, (count + 1) * sizeof(char *));
@@ -138,6 +154,11 @@ static void read_file(FILE *fp) {
     char **dests;
     short *valve_dests;
 
+#ifdef __CC65__
+    gotoxy(1,1);
+    printf("Computing paths from %s: ", valve_name[count]);
+#endif
+
     num_dest = strsplit(valve_destinations_str[count], ' ', &dests);
     valve_dests = malloc(num_dest*sizeof(short));
 
@@ -147,9 +168,16 @@ static void read_file(FILE *fp) {
       if (strchr(dests[i],'\n'))
         *strchr(dests[i],'\n') = '\0';
 
+#ifdef __CC65__
+    printf("%s ", dests[i]);
+#endif
       valve_dests[i] = get_valve_by_name(dests[i]);
       free(dests[i]);
     }
+#ifdef __CC65__
+    printf("                              ");
+#endif
+
     bfs_add_paths(b, count, valve_dests, num_dest);
 
     free(valve_destinations_str[count]);
@@ -164,19 +192,29 @@ static void read_file(FILE *fp) {
   enabled_targets = malloc(num_valves * sizeof(short));
 
   /* Do the thing */
+#ifdef __CC65__
+  clrscr();
+#endif
   start_valve = get_valve_by_name("AA");
   for (count = 0; count < num_valves; count++) {
     if (valve_flow[count] > 0 || count == start_valve) {
       targets = realloc(targets, (num_targets + 1) * sizeof(short));
       targets[num_targets] = count;
       num_targets++;
+#ifdef __CC65__
+      gotoxy(1,1);
+      printf("BFS shortest paths for %s... (%d/%d)\n", valve_name[count], count, num_valves);
+#endif
       bfs_dists[count] = bfs_compute_shortest_paths(b, count);
       enabled_targets[count] = 1;
     } else {
       enabled_targets[count] = 0;
     }
   }
-
+  
+#ifdef __CC65__
+  clrscr();
+#endif
   printf("best flow in part 1: %d\n", find_optimal_flow(start_valve, 30, bfs_dists, targets, num_targets, enabled_targets, 0));
   
   /* free; keep Valgrind happy */
