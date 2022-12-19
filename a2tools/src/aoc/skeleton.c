728cffad019f998ccf2a22f42bb002effcbdbe5c
Colin Leroy-Mira <colin@colino.net>
2022-12-19 08:24:15 +0100

    That'll be easier than cp -r

---
+++ a2tools/src/aoc/skeleton.c
@@ -0,0 +1,44 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <errno.h>
+#include <unistd.h>
+#ifdef __CC65__
+#include <apple2.h>
+#endif
+
+#define BUFSIZE 255
+static void read_file(FILE *fp);
+
+#define DATASET "IN16E"
+
+int main(void) {
+  FILE *fp;
+
+#ifdef PRODOS_T_TXT
+  _filetype = PRODOS_T_TXT;
+#endif
+  fp = fopen(DATASET, "r");
+  if (fp == NULL) {
+    printf("Error %d\n", errno);
+    exit(1);
+  }
+
+  read_file(fp);
+
+  exit (0);
+}
+
+static void read_file(FILE *fp) {
+  char *buf = malloc(BUFSIZE);
+
+  while (fgets(buf, BUFSIZE-1, fp) != NULL) {
+
+
+  };
+
+  printf("\n");
+
+  free(buf);
+  fclose(fp);
+}
