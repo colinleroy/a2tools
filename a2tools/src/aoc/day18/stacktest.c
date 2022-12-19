60732d3b7bc47043562b50b51c02121b54040cdd
Colin Leroy-Mira <colin@colino.net>
2022-12-19 08:21:12 +0100

    Working on the //c!

---
+++ a2tools/src/aoc/day18/stacktest.c
@@ -0,0 +1,33 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <errno.h>
+#include <unistd.h>
+#ifdef __CC65__
+#include <apple2.h>
+#endif
+
+#define BUFSIZE 255
+static void read_file(FILE *fp);
+
+#define DATASET "IN16E"
+
+int stack_size_test(void *ptr, int x, int y, int z, int d) {
+  char result = 0;
+  printf("%d.", d);
+  return result || stack_size_test(ptr, x, y, z, d + 1);
+}
+
+int main(void) {
+  char *buf;
+  int i, j;
+  char ***cubes = NULL;
+  int num_cubes = 0;
+  int surface_area = 0;
+  int exterior_surface_area = 0;
+  char *s_x, *s_y, *s_z;
+  int x, y, z;
+  
+  cubes = malloc(16000);
+  return stack_size_test(cubes, 1, 2, 3, 0);
+}
