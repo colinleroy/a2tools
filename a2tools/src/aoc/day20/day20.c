7f0bcc8808b1ea8034763f498a1d4c510472d614
Colin Leroy-Mira <colin@colino.net>
2022-12-20 18:25:09 +0100

    Ready for production

--- a2tools/src/aoc/day20/day20.c
+++ a2tools/src/aoc/day20/day20.c
@@ -6,7 +6,7 @@
 #ifdef __CC65__
 #include <apple2.h>
 #endif
-#include "bool_array.h"
+#include "extended_conio.h"
 
 #define BUFSIZE 255
 static void read_file(FILE *fp);
@@ -38,28 +38,37 @@ struct _item {
   item *original_next;
 };
 
-void debug_array(item *cur) {
+void debug_array(item *cur, int n, int num_lines) {
     item *start = cur;
     int i;
-    for (i = 0; i < 3; i++)
-      start = start->prev;
-    for (i = 0; i < 6; i++) {
-      printf("%5d ", start->value);
-      start = start->next;
+    if (n < 10 || 
+       (n < 100 && n % 10 == 0) ||
+       (n % 100 == 0) ||
+       n == num_lines) {
+      for (i = 0; i < 3; i++)
+        start = start->prev;
+        
+      gotoxy(0, 10);
+      for (i = 0; i < 6; i++) {
+        printf("%5d ", start->value);
+        start = start->next;
+      }
+      printf("\n\n%5d shifts/%5d done\n", n, num_lines);
     }
-    printf("\n");
 }
 
 static void read_file(FILE *fp) {
   char *buf = malloc(BUFSIZE);
   item *values = NULL;
-  int i = 0, shifted = 0, j = 0, sum = 0;
+  int i = 0, shifted = 0, num_lines = 0, sum = 0;
   item *cur, *start, *zero, *last_read = NULL;
   item *next_to_shift;
 
+  clrscr();
   while (fgets(buf, BUFSIZE-1, fp) != NULL) {
     values = malloc(sizeof(struct _item));
     values->value = atoi(buf);
+    num_lines++;
     if (i == 0) {
       start = values;
     } else {
@@ -68,6 +77,10 @@ static void read_file(FILE *fp) {
       last_read->original_next = values;
     }
     last_read = values;
+    if (i % 100 == 0) {
+      gotoxy(0,0);
+      printf("Reading... %d lines...\n", i);
+    }
     i++;
   }
   free(buf);
@@ -78,9 +91,10 @@ static void read_file(FILE *fp) {
   start->prev = last_read;
 
   cur = start;
-  debug_array(cur);
+  debug_array(cur, 0, num_lines);
   do {
     int shift, rem;
+
     if (cur->value == 0) {
       zero = cur;
     }
@@ -88,10 +102,15 @@ static void read_file(FILE *fp) {
 
     shift = cur->value;
     rem = shift;
+    /* Yes I suck at doubly-linked lists
+     * so I'm going to shift one slot at
+     * a time and you're not going to do
+     * anything about it.
+     */
     while (rem > 0) {
-      item *prev;
-      item *next;
-      item *after_next;
+      item *prev = cur->prev;
+      item *next = cur->next;
+      item *after_next = next->next;
 
       prev = cur->prev;
       next = cur->next;
@@ -113,7 +132,6 @@ static void read_file(FILE *fp) {
       cur->next = after_next;
       after_next->prev = cur;
       
-      //debug_array(cur);
       rem--;
     }
     while (rem < 0) {
@@ -137,12 +155,12 @@ static void read_file(FILE *fp) {
       before_prev->next = cur;
       cur->prev = before_prev;
       
-      //debug_array(cur);
       rem++;
     }
+    shifted++;
+    debug_array(cur, shifted, num_lines);
 
     cur = next_to_shift;
-    shifted++;
   } while (next_to_shift != NULL);
   printf("\n");
   
@@ -150,14 +168,15 @@ static void read_file(FILE *fp) {
 
   for (i = 0; i < 3001; i++) {
     if (i > 0 && i % 1000 == 0) {
-      debug_array(cur);
+      debug_array(cur, num_lines, num_lines);
 
       sum += cur->value;
+      gotoxy(0, 15);
       printf("Adding %d at %d\n", cur->value, i);
     }
     cur = cur->next;
   }
 
-  printf("value: %d\n", sum);
+  printf("Code is: %d\n", sum);
 
 }
