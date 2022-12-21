69e5a4f3679bc6b6126f0e82e446a9c2b5e80cf8
Colin Leroy-Mira <colin@colino.net>
2022-12-21 08:03:34 +0100

    Optimize quite a bit

--- a2tools/src/aoc/day20/day20.c
+++ a2tools/src/aoc/day20/day20.c
@@ -40,22 +40,35 @@ struct _item {
 
 void debug_array(item *cur, int n, int num_lines) {
     item *start = cur;
-    int i;
-    if (n < 10 || 
-       (n < 100 && n % 10 == 0) ||
-       (n % 100 == 0) ||
-       n == num_lines) {
-      for (i = 0; i < 3; i++)
-        start = start->prev;
-        
-      gotoxy(0, 10);
-      for (i = 0; i < 6; i++) {
-        printf("%5d ", start->value);
-        start = start->next;
-      }
-      printf("\n\n%5d shifts/%5d done\n", n, num_lines);
+    int i = 0;
+    return;
+    while (i ++ < num_lines) {
+      printf("%5d\n", cur->value);
+      cur = cur->next;
     }
+    printf("\n");
+    cgetc();
+    // if (n < 10 || 
+    //    (n < 100 && n % 10 == 0) ||
+    //    (n % 100 == 0) ||
+    //    n == num_lines) {
+    //   for (i = 0; i < 3; i++)
+    //     start = start->prev;
+    // 
+    //   gotoxy(0, 10);
+    //   for (i = 0; i < 6; i++) {
+    //     printf("%5d ", start->value);
+    //     start = start->next;
+    //   }
+    //   printf("\n\n%5d shifts/%5d done\n", n, num_lines);
+    // }
+    
 }
+
+static item *remove_after;
+static item *remove_before;
+static item *insert_before;
+static item *insert_after;
 
 static void read_file(FILE *fp) {
   char *buf = malloc(BUFSIZE);
@@ -102,69 +115,67 @@ static void read_file(FILE *fp) {
 
     shift = cur->value;
     rem = shift;
-    /* Yes I suck at doubly-linked lists
-     * so I'm going to shift one slot at
-     * a time and you're not going to do
-     * anything about it.
-     */
-    while (rem > 0) {
-      item *prev = cur->prev;
-      item *next = cur->next;
-      item *after_next = next->next;
 
-      if (cur == start) {
-        start = next;
-      }
-
-      cur->prev = NULL;
-      cur->next = NULL;
-      
-      prev->next = next;
-      next->prev = prev;
-      
-      next->next = cur;
-      cur->prev = next;
-      
-      cur->next = after_next;
-      after_next->prev = cur;
-      
-      rem--;
+    if (rem == 0)
+      goto nothing_to_do;
+    /* handle us leaving start */
+    if (cur == start) {
+      start = rem > 0 ? start->next : start->prev;
     }
-    while (rem < 0) {
-      item *prev = cur->prev;
-      item *next = cur->next;
-      item *before_prev = prev->prev;
 
-      if (cur == start) {
-        start = prev;
+    remove_after = cur->prev;
+    remove_before = cur->next;
+
+    insert_before = rem > 0 ? cur->next : cur->prev;
+    /* Pluck it for where we are */
+    cur->prev = NULL;
+    cur->next = NULL;
+
+    /* Join RA-RB */
+    remove_after->next = remove_before;
+    remove_before->prev = remove_after;
+
+    if (rem > 0) {
+      for (i = 0; i < rem; i++) {
+        insert_before = insert_before->next;
       }
-
-      cur->prev = NULL;
-      cur->next = NULL;
-
-      next->prev = prev;
-      prev->next = next;
-
-      prev->prev = cur;
-      cur->next = prev;
-      
-      before_prev->next = cur;
-      cur->prev = before_prev;
-      
-      rem++;
     }
+    if (rem < 0) {
+      for (i = rem; i < -1; i++) {
+        insert_before = insert_before->prev;
+      }
+    }
+
+    insert_after = insert_before->prev;
+    
+    /* we are now like:
+     *  - IA - x - IB - ... - RA - CUR - RB - ... */
+    
+    /* handle us taking start's spot */
+    if (insert_before->prev == start) {
+      start = cur;
+    }
+    
+    /* reinsert where we should */
+    insert_after->next = cur;
+    cur->next = insert_before;
+    insert_before->prev = cur;
+    cur->prev = insert_after;
+
+nothing_to_do:
     shifted++;
-    debug_array(cur, shifted, num_lines);
+    debug_array(start, shifted, num_lines);
 
     cur = next_to_shift;
   } while (next_to_shift != NULL);
+  
   printf("\n");
   
   cur = zero;
 
   for (i = 0; i < 3001; i++) {
     if (i > 0 && i % 1000 == 0) {
-      debug_array(cur, num_lines, num_lines);
+      debug_array(start, num_lines, num_lines);
 
       sum += cur->value;
       gotoxy(0, 15);
