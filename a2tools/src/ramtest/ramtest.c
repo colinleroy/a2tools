6e277408b53cb6da8aa74c12982116d20c30f1a9
Colin Leroy-Mira <colin@colino.net>
2022-12-23 23:46:39 +0100

    Remove comment

--- a2tools/src/ramtest/ramtest.c
+++ a2tools/src/ramtest/ramtest.c
@@ -8,19 +8,13 @@
 #endif
 #include "extended_conio.h"
 
-#define BUFSIZE 255
-static void read_file(FILE *fp);
-
-#define DATASET "IN16E"
-
 int main(void) {
   unsigned int total = 0;
-
   while (1) {
     char *buf;
 
     printf("Heap avail %u, max block %u\n", _heapmemavail(), _heapmaxavail());
-    
+    cgetc();  
     buf = malloc(1024);
     if (buf != NULL) {
       memset(buf, 0, 1024);
