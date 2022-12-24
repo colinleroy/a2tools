04724e9a2129528bf32af84210c812547f61ddbd
Colin Leroy-Mira <colin@colino.net>
2022-12-24 13:39:32 +0100

    leak fix

--- a2tools/src/lib/bfs.h
+++ a2tools/src/lib/bfs.h
@@ -11,12 +11,12 @@ void bfs_add_nodes(bfs *b, int num_nodes);
 void bfs_add_paths(bfs *b, short source, short *dest_nodes, int num_dests);
 
 /* Not to be freed */
-const short *bfs_compute_shortest_paths(bfs *b, short start_node);
+const short *bfs_compute_shortest_distances(bfs *b, short start_node);
 
 /* helper for grid. Use instead of bfs_add_nodes */
 void bfs_set_grid(bfs *b, int max_x, int max_y);
 int bfs_grid_to_node(bfs *b, int x, int y);
 void bfs_grid_add_paths(bfs *b, int x, int y, short *dest_nodes, int num_dests);
-const short *bfs_grid_compute_shortest_paths(bfs *b, int x, int y);
-short bfs_grid_get_shortest_path_to(bfs *b, int start_x, int start_y, int end_x, int end_y);
+const short *bfs_grid_compute_shortest_distances(bfs *b, int x, int y);
+short bfs_grid_get_shortest_distance_to(bfs *b, int start_x, int start_y, int end_x, int end_y);
 #endif
