55c182cab1acddfb523ae8bce7f1f03c531a555f
Colin Leroy-Mira <colin@colino.net>
2023-01-03 20:28:14 +0100

    auto open serial, error paths fixes

--- src/lib/http.c
+++ src/lib/http.c
@@ -11,37 +11,49 @@
 #include "http_curl.c"
 
 #else
-
+static char proxy_opened = 0;
 void http_connect_proxy(void) {
 #ifdef __CC65__
   simple_serial_open(2, SER_BAUD_9600);
   simple_serial_set_timeout(10);
 #endif
+  proxy_opened = 1;
 }
 
 static char buf[BUFSIZE];
 
 http_response *http_request(const char *method, const char *url, const char **headers, int n_headers) {
-  http_response *resp = malloc(sizeof(http_response));
+  http_response *resp;
   int i;
 
+  if (proxy_opened == 0) {
+    http_connect_proxy();
+  }
+
+  resp = malloc(sizeof(http_response));
+  if (resp == NULL) {
+    return NULL;
+  }
+
+  resp->body = NULL;
+  resp->size = 0;
+  resp->code = 0;
+
   simple_serial_printf("%s %s\n", method, url);
+
   for (i = 0; i < n_headers; i++) {
     simple_serial_printf("%s\n", headers[i]);
+    printf("sent header %s.\n", headers[i]);
   }
   simple_serial_puts("\n");
 
   simple_serial_gets_with_timeout(buf, BUFSIZE);
   if (buf == NULL || buf[0] == '\0') {
-    resp->size = 0;
-    resp->code = 0;
     return resp;
   }
 
   if (strchr(buf, ',') == NULL) {
     printf("Unexpected response '%s'\n", buf);
-    resp->size = 0;
-    resp->code = 0;
     return resp;
   }
 
