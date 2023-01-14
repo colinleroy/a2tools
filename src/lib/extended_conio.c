622995687823524bd1a4d8a6d11ec2b4ba93f27a
Colin Leroy-Mira <colin@colino.net>
2023-01-14 18:58:55 +0100

    implement wherex/y on posix

--- src/lib/extended_conio.c
+++ src/lib/extended_conio.c
@@ -84,6 +84,7 @@ void cputsxy(int x, int y, char *buf) {
 
   gotoxy(x, y);
   printf("%s", buf);
+  fflush(stdout);
 }
 
 void screensize(unsigned char *w, unsigned char *h) {
@@ -268,3 +269,63 @@ void set_scrollwindow(unsigned char top, unsigned char bottom) {
 
 #endif
 }
+
+#ifndef __CC65__
+
+static int get_pos(int *y, int *x) {
+
+ char buf[30]={0};
+ int ret, i, pow;
+ char ch;
+
+*y = 0; *x = 0;
+
+ struct termios term, restore;
+
+ tcgetattr(0, &term);
+ tcgetattr(0, &restore);
+ term.c_lflag &= ~(ICANON|ECHO);
+ tcsetattr(0, TCSANOW, &term);
+
+ write(1, "\033[6n", 4);
+
+ for( i = 0, ch = 0; ch != 'R'; i++ )
+ {
+    ret = read(0, &ch, 1);
+    if ( !ret ) {
+       tcsetattr(0, TCSANOW, &restore);
+       fprintf(stderr, "getpos: error reading response!\n");
+       return 1;
+    }
+    buf[i] = ch;
+ }
+
+ if (i < 2) {
+    tcsetattr(0, TCSANOW, &restore);
+    printf("i < 2\n");
+    return(1);
+ }
+
+ for( i -= 2, pow = 1; buf[i] != ';'; i--, pow *= 10)
+     *y = *y + ( buf[i] - '0' ) * pow;
+
+ for( i-- , pow = 1; buf[i] != '['; i--, pow *= 10)
+     *x = *x + ( buf[i] - '0' ) * pow;
+
+ tcsetattr(0, TCSANOW, &restore);
+ return 0;
+}
+int wherex(void) {
+  int x = 0, y = 0;
+  get_pos(&x, &y);
+
+  return x - 1;
+}
+
+int wherey(void) {
+  int x = 0, y = 0;
+  get_pos(&x, &y);
+
+  return y - 1;
+}
+#endif
