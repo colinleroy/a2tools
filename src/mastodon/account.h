45615d23bb88e268c63ddb067d3d33a07f7d6f1b
Colin Leroy-Mira <colin@colino.net>
2023-01-21 18:33:07 +0100

    I can fetch posts!

---
+++ src/mastodon/account.h
@@ -0,0 +1,16 @@
+#ifndef __account_h
+#define __account_h
+
+typedef struct _account account;
+
+struct _account {
+  char *id;
+  char *username;
+  char *display_name;
+};
+
+account *account_new(void);
+account *account_new_from_status_json(surl_response *resp, char *id, char is_reblog);
+void account_free(account *a);
+
+#endif
