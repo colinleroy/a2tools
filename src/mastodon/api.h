881a1baec31ee7b8e480ab92a1aca4cdc1e8a650
Colin Leroy-Mira <colin@colino.net>
2023-01-22 11:17:06 +0100

    OK memory's sparse as usual

--- src/mastodon/api.h
+++ src/mastodon/api.h
@@ -4,7 +4,10 @@
 #include "status.h"
 #include "account.h"
 
-void api_get_profile(char **public_name, char **handle);
+#define SELECTOR_SIZE 128
+extern char selector[SELECTOR_SIZE];
+
+int api_get_profile(char **public_name, char **handle);
 
 #define HOME_TIMELINE "home"
 int api_get_timeline_posts(char *tlid, status ***posts);
--- src/mastodon/status.c
+++ src/mastodon/status.c
@@ -6,12 +6,17 @@
 #include "simple_serial.h"
 #include "extended_conio.h"
 #include "extended_string.h"
-#include "status.h"
+#include "api.h"
 
 #define BUF_SIZE 255
+#define TL_STATUS_MAX_LEN 256
 
 status *status_new(void) {
   status *s = malloc(sizeof(status));
+  if (s == NULL) {
+    printf("No more memory at %s:%d\n",__FILE__, __LINE__);
+    return NULL;
+  }
   memset(s, 0, sizeof(status));
   return s;
 }
@@ -20,22 +25,37 @@ status *status_new(void) {
 #pragma static-locals (push,off) /* need reentrancy */
 #endif
 status *status_new_from_json(surl_response *resp, char *id, char is_reblog) {
-  char *selector;
   status *s;
   char *reblog_id;
 
   s = status_new();
   s->id = strdup(id);
+  if (s->id == NULL) {
+    printf("No more memory at %s:%d\n",__FILE__, __LINE__);
+  }
+
   s->created_at = malloc(26);
-  s->content = malloc(1024);
-  
-  selector = malloc(BUF_SIZE);
-  snprintf(selector, BUF_SIZE, ".[]|select(.id==\"%s\").%screated_at", id, is_reblog?"reblog.":"");
+  if (s->created_at == NULL) {
+    printf("No more memory at %s:%d\n",__FILE__, __LINE__);
+    return NULL;
+  }
+  s->content = malloc(TL_STATUS_MAX_LEN);
+  if (s->content == NULL) {
+    printf("No more memory at %s:%d\n",__FILE__, __LINE__);
+    return NULL;
+  }
+
+  snprintf(selector, SELECTOR_SIZE, ".[]|select(.id==\"%s\").%screated_at", id, is_reblog?"reblog.":"");
   surl_get_json(resp, s->created_at, 26, 0, 0, selector);
 
   if (!is_reblog) {
-    reblog_id = malloc(BUF_SIZE);
-    snprintf(selector, BUF_SIZE, ".[]|select(.id==\"%s\").reblog.id", id);
+    reblog_id = malloc(26);
+    if (reblog_id == NULL) {
+      printf("No more memory at %s:%d\n",__FILE__, __LINE__);
+      return NULL;
+    }
+
+    snprintf(selector, SELECTOR_SIZE, ".[]|select(.id==\"%s\").reblog.id", id);
     surl_get_json(resp, reblog_id, BUF_SIZE, 0, 0, selector);
     if (reblog_id[0] != '\0') {
       s->reblog = status_new_from_json(resp, id, 1);
@@ -48,14 +68,15 @@ status *status_new_from_json(surl_response *resp, char *id, char is_reblog) {
   }
 
   if (s->reblog == NULL) {
-    snprintf(selector, BUF_SIZE, ".[]|select(.id==\"%s\").%scontent", id, is_reblog?"reblog.":"");
-    surl_get_json(resp, s->content, 1024, 1, 1, selector);
+    snprintf(selector, SELECTOR_SIZE, ".[]|select(.id==\"%s\").%scontent", id, is_reblog?"reblog.":"");
+    surl_get_json(resp, s->content, TL_STATUS_MAX_LEN, 1, 1, selector);
+    s->content[TL_STATUS_MAX_LEN - 4] = '.';
+    s->content[TL_STATUS_MAX_LEN - 3] = '.';
+    s->content[TL_STATUS_MAX_LEN - 2] = '.';
   } else {
     free(s->content);
     s->content = NULL;
   }
-
-  free(selector);
 
   s->account = account_new_from_status_json(resp, id, is_reblog);
   return s;
