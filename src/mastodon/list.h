437b790c821e476be0b12982473b14043af41664
Colin Leroy-Mira <colin@colino.net>
2023-01-23 07:44:39 +0100

    move list var to list struct

---
+++ src/mastodon/list.h
@@ -0,0 +1,22 @@
+#ifndef __list_h
+#define __list_h
+
+#include <string.h>
+
+typedef struct _list list;
+
+#define L_HOME_TIMELINE   0
+#define L_LOCAL_TIMELINE  1
+#define L_PUBLIC_TIMELINE 2
+#define L_FULL_STATUS     3
+
+struct _list {
+  char kind;
+  char **ids;
+  char first_displayed_post;
+  status **displayed_posts;
+  char last_displayed_post;
+  signed char *post_height;
+  char n_posts;
+};
+#endif
