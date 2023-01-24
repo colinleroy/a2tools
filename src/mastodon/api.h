79e8b9764daebe9dcb3610a81873e36e6c28e901
Colin Leroy-Mira <colin@colino.net>
2023-01-24 22:25:55 +0100

    Account view, lots of cleanups.

--- src/mastodon/api.h
+++ src/mastodon/api.h
@@ -6,12 +6,19 @@
 
 #define SELECTOR_SIZE 255
 extern char selector[SELECTOR_SIZE];
+#define BUF_SIZE 255
+extern char gen_buf[BUF_SIZE];
 
 account *api_get_profile(char *id);
 
-#define HOME_TIMELINE "home"
-int api_get_timeline_posts(char *tlid, char to_load, char *last_to_load, char *first_to_load, char **post_ids);
+#define ACCOUNTS_ENDPOINT "/api/v1/accounts/"
+#define TIMELINE_ENDPOINT "/api/v1/timelines/"
+#define STATUS_ENDPOINT   "/api/v1/statuses/"
 
+#define HOME_TIMELINE "home"
+
+int api_get_posts(char *endpoint, char to_load, char *first_to_load, char **post_ids);
+int api_get_account_posts(account *a, char to_load, char *first_to_load, char **post_ids);
 int api_get_status_and_replies(char to_load, status *root, char **post_ids);
 
 status *api_get_status(char *post_id, char full);
@@ -19,4 +26,7 @@ status *api_get_status(char *post_id, char full);
 void api_favourite_status(status *s);
 void api_reblog_status(status *s);
 char api_delete_status(status *s);
+
+char api_relationship_get(account *a, char f);
+account *api_get_full_account(account *a);
 #endif
