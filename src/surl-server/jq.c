57bdd70ebbf4924cb4998bf15e8d28eca1f2c956
Colin Leroy-Mira <colin@colino.net>
2023-02-04 16:26:33 +0100

    Optimize curl and jq

--- src/surl-server/jq.c
+++ src/surl-server/jq.c
@@ -3,29 +3,40 @@
 #include <errno.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <time.h>
+
 #include <jq.h>
 #include "jq.h"
 
-char *jq_get(char *buffer, char *selector) {
-  jq_state *jq = NULL;
-  jv json_data;
+char out[8192];
+char *jq_get(jv json_data, char *selector) {
+  static jq_state *jq = NULL;
   jv result;
-  char *out = NULL;
   size_t out_len = 0;
+  long start_secs = 0;
+  long start_msecs = 0;
+  long secs = 0;
+  long msecs = 0;
+  struct timespec cur_time;
 
-  jq = jq_init();
+  out[0] = '\0';
+
+  clock_gettime(CLOCK_REALTIME, &cur_time);
+  start_secs = cur_time.tv_sec - 1;
+  start_msecs = 1000 + (cur_time.tv_nsec / 1000000);
+
+  if (jq == NULL) {
+    jq = jq_init();
+  }
   if (jq == NULL) {
     return NULL;
   }
 
-  json_data = jv_parse(buffer);
   if (!jv_is_valid(json_data)) {
-    jq_teardown(&jq);
     return NULL;
   }
 
   if (!jq_compile(jq, selector)) {
-    jq_teardown(&jq);
     return NULL;
   }
 
@@ -33,7 +44,7 @@ char *jq_get(char *buffer, char *selector) {
   
   while (jv_is_valid(result = jq_next(jq))) {
     jv str;
-    char *tmp;
+    const char *tmp;
     if (jv_get_kind(result) == JV_KIND_NULL
      || jv_get_kind(result) == JV_KIND_INVALID) {
        continue;
@@ -41,25 +52,29 @@ char *jq_get(char *buffer, char *selector) {
 
     if (jv_get_kind(result) != JV_KIND_STRING) {
       str = jv_dump_string(result, 0);
-      tmp = strdup(jv_string_value(str));
+      tmp = jv_string_value(str);
+      strcpy(out + out_len, tmp);
+      out_len += strlen(tmp);
       jv_free(str);
     } else {
-      tmp = strdup(jv_string_value(result));
+      tmp = jv_string_value(result);
+      strcpy(out + out_len, tmp);
+      out_len += strlen(tmp);
       jv_free(result);
     }
-
-    out = realloc(out, out_len + strlen(tmp) + 2);
-    strcpy(out + out_len, tmp);
-    out_len += strlen(tmp);
-
-    free(tmp);
 
     out[out_len++] = '\n';
     out[out_len] = '\0';
   }
 
   jv_free(result);
-  jq_teardown(&jq);
 
-  return out;
+  clock_gettime(CLOCK_REALTIME, &cur_time);
+  secs = cur_time.tv_sec - 1;
+  msecs = 1000 + (cur_time.tv_nsec / 1000000);
+  
+  printf("jq_get %s %lums\n", selector,
+      (1000*(secs - start_secs))+(msecs - start_msecs));
+
+  return strdup(out);
 }
