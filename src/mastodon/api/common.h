1b8ca0f9eb0914bfc115b33ce9d2681ba96615ad
Colin Leroy-Mira <colin@colino.net>
2023-07-12 13:42:01 +0200

    Add other known keyboard layouts

--- src/mastodon/api/common.h
+++ src/mastodon/api/common.h
@@ -14,9 +14,14 @@ extern char endpoint_buf[ENDPOINT_BUF_SIZE];
 extern char *tl_endpoints[3];
 extern char *tl_filter[3];
 
-/* FIXME make that configurable */
+/* Refs: https://www.aivosto.com/articles/charsets-7bit.html
+ * and https://archive.org/details/Apple_IIgs_Hardware_Reference_HiRes/page/n275/mode/1up?view=theater
+ * FIXME make that configurable */
 #define US_CHARSET "US-ASCII"
 #define FR_CHARSET "ISO646-FR1"
+#define ES_CHARSET "ISO646-ES"
+#define IT_CHARSET "ISO646-IT"
+#define DE_CHARSET "ISO646-DE"
 
 #define SELECTOR_SIZE 255
 extern char selector[SELECTOR_SIZE];
