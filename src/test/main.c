caa4ef004447ea7c8d62e07d4aa652ab96314b04
Colin Leroy-Mira <colin@colino.net>
2023-08-18 14:23:58 +0200

    test cc65 fix

--- src/test/main.c
+++ src/test/main.c
@@ -5,22 +5,12 @@
 #include <stdarg.h>
 #include <errno.h>
 #include <conio.h>
+#include <unistd.h>
 #include "clrzone.h"
 
 int main(int argc, char *argv[]) {
-  char x, y;
-  
-  videomode(VIDEOMODE_80COL);
-  for (x = 0; x < 80; x++) {
-    for (y = 0; y < 24; y++) {
-      cputcxy(x, y, '*');
-    }
-  }
+  char buf[100];
+  getcwd(buf, 100);
+  printf("%s, %zuB free\n", buf, _heapmemavail());
   cgetc();
-  gotoxy(0, 0);
-  
-  clrzone(2, 2, 78, 2);
-  cgetc();
-
-  return 0;
 }
