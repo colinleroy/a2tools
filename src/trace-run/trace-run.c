fcb27fa3adc7f59309ce77e7974ab2620de270ad
Colin Leroy-Mira <colin@colino.net>
2023-08-26 23:06:55 +0200

    Handle A/X/Y registers

--- src/trace-run/trace-run.c
+++ src/trace-run/trace-run.c
@@ -60,11 +60,45 @@ static void tabulate(const char *buf, int len) {
       printf(" ");
   }
 }
+
+static int detect_tracelog(char *line) {
+  char checkbuf[32];
+  if (strlen(line) == 4 && hex2int(line) > 0) {
+    fprintf(stderr, "Found addresses at index 0\n");
+    return 0;
+  }
+  if(strlen(line) > 4 && strchr(line, ' ')) {
+    char *try_addr = strchr(line, ' ') + 1;
+    int val = hex2int(try_addr);
+
+    snprintf(checkbuf, 5, "%04X", val);
+    if (!strcmp(try_addr, checkbuf)) {
+      int idx = (int)(try_addr - line);
+      fprintf(stderr, "Found addresses at index %d\n", idx);
+      return idx;
+    }
+  }
+  fprintf(stderr, "%s\n", line);
+  fprintf(stderr, "Can not parse log (address field not found)\n");
+  exit(1);
+}
+
+static void update_regs(const char *buf, int *a, int *x, int *y) {
+  *a = *x = *y = 0;
+  if (strstr(buf, "A="))
+    *a = hex2int(strstr(buf, "A=") + 2);
+  if (strstr(buf, "X="))
+    *x = hex2int(strstr(buf, "X=") + 2);
+  if (strstr(buf, "Y="))
+    *y = hex2int(strstr(buf, "Y=") + 2);
+}
+
 static void annotate_run(const char *file) {
   FILE *fp = fopen(file, "r");
   char buf[BUF_SIZE];
   char line_buf[BUF_SIZE];
   int cur_line = 0;
+  int op_idx = -1;
 
   if (fp == NULL) {
     fprintf(stderr, "Can not open file %s: %s\n", file, strerror(errno));
@@ -113,6 +147,10 @@ skip_to_start:
 
     n_parts = strsplit_in_place(line, ':', &parts);
 
+    if (op_idx == -1) {
+      op_idx = detect_tracelog(line);
+    }
+
     /* We want at least an "ADDR: op" */
     if (n_parts >= 2) {
       int op_addr = 0;
@@ -122,9 +160,11 @@ skip_to_start:
       dbg_symbol *param_symbol = NULL;
       char *instr, *arg;
       char comment[BUF_SIZE];
+      char * cur_lineaddress = parts[0] + op_idx;
+      int a, x, y;
 
       /* get the op's address */
-      op_addr = hex2int(parts[0]);
+      op_addr = hex2int(cur_lineaddress);
 
       /* If we've not yet seen the start address,
        * skip line
@@ -170,7 +210,7 @@ skip_to_start:
         }
         instr_symbol = symbol_get_by_addr(op_addr, read_from, lc_bank);
         if (!instr_symbol) {
-          instr_symbol = generate_symbol(parts[0], op_addr, read_from, lc_bank, NULL);
+          instr_symbol = generate_symbol(cur_lineaddress, op_addr, read_from, lc_bank, NULL);
         }
       }
 
@@ -187,18 +227,37 @@ skip_to_start:
       if (arg && arg[0] == '$') {
         if (strchr(arg, '\n'))
           *strchr(arg, '\n') = '\0';
+
+        param_addr = 0;
+        if (op_idx > 0 && (strstr(arg, ", x") || strstr(arg, ", y"))) {
+          /* Figure out A X Y */
+          update_regs(parts[0], &a, &x, &y);
+        }
+        /* calculate offset */
+        if (strstr(arg, ", x")) {
+          param_addr = x;
+        }
+        if (strstr(arg, ", y")) {
+          param_addr = y;
+        }
+
         if (strchr(arg, ','))
           *strchr(arg, ',') = '\0';
 
-        param_addr = hex2int(arg + 1); /* skip $ */
+        param_addr += hex2int(arg + 1); /* skip $ */
 
         if (param_addr >= 0) {
           int dest = is_instruction_write(instr) ? write_to : read_from;
           param_symbol = symbol_get_by_addr(param_addr, dest, lc_bank);
 
           /* If we don't have a symbol, generate a dummy one */
-          if (param_symbol == NULL)
-            goto try_gen;
+          if (param_symbol == NULL) {
+            int dest = is_instruction_write(instr) ? write_to : read_from;
+            char new_str_addr[10];
+            snprintf(new_str_addr, 10, "%04X", param_addr);
+            param_symbol = generate_symbol(new_str_addr, param_addr, dest, lc_bank, n_parts > 2 ? parts[2] : NULL);
+            param_addr = symbol_get_addr(param_symbol);
+          }
         } else {
           goto try_gen;
         }
@@ -213,8 +272,13 @@ try_gen:
       }
 
       /* Print the line as-is */
-      if (!do_callgrind)
+      if (!do_callgrind) {
         printf("%s", line_buf);
+        if (strlen(line_buf) > op_idx + 18) {
+          printf("\n");
+          tabulate("", op_idx + 18);
+        }
+      }
 
       /* Profile if needed */
       if (do_callgrind && update_call_counters(op_addr, instr, param_addr, cycles, cur_line) < 0) {
@@ -225,13 +289,14 @@ try_gen:
       if (analyze_instruction(op_addr, instr, param_addr, comment)
        || (!sloc && !instr_symbol && !param_symbol)) {
         /* print either banking comment or finish the line if we have zero data about it */
-        if (!do_callgrind)
-          printf("%s\n", comment);
-
+        if (!do_callgrind) {
+          tabulate(line_buf, op_idx + 18);
+          printf("; *%s*\n", comment);
+        }
       } else {
         if (!do_callgrind) {
-          tabulate(line_buf, 18);
-          printf("; OP: ");
+          tabulate(line_buf, op_idx + 18);
+          printf("; adr: ");
         }
         /* Display the instruction's symbol */
         if (instr_symbol) {
@@ -245,7 +310,7 @@ try_gen:
 
         /* Print the argument's symbol */
         if (!do_callgrind)
-          printf("ARG: ");
+          printf("%s ", instr);
         if (param_symbol) {
           if (!do_callgrind) {
             printf("%s", symbol_get_name(param_symbol));
@@ -262,7 +327,7 @@ try_gen:
 
         /* Print the source location */
         if (sloc && !do_callgrind) {
-          printf("OP loc: %s:%d", sloc_get_filename(sloc), sloc_get_line(sloc));
+          printf("(%s:%d)", sloc_get_filename(sloc), sloc_get_line(sloc));
         }
         if (!do_callgrind) 
           printf("\n");
@@ -302,12 +367,18 @@ int main(int argc, char *argv[]) {
 
   if (argc < 5) {
 err_usage:
-    fprintf(stderr, 
+    fprintf(stderr,
+           "This tool is quite limited to interpreting CPU trace logs under certain\n"
+           "conditions:\n"
+           " - It reads trace files generated by MAME, expecting a certain format\n"
+           " - For programs compiled using cc65 (`ld65 -g -Wl --dbgFile,program.dbg`)\n"
+           " - For programs running on Apple2.\n"
+           "\n"
            "Usage: %s -d cc65_dbg_file -l cc65_lbl_file -t mame_trace_file [-f]\n"
            "\n"
-           "-d  cc65_dbg_file  : point to a cc65-generated .dbg file\n"
-           "-l  cc65_lbl_file  : point to a cc65-generated .lbl file\n"
-           "-t  mame_trace_file: point to a MAME debugger generated trace file\n"
+           "-d  file.dbg       : point to a cc65-generated .dbg file (best)\n"
+           "-l  file.lbl       : or point to a cc65-generated .lbl file (inferior)\n"
+           "-t  file.tr        : point to a MAME debugger generated trace file\n"
            "-x  start_addr     : specify start address, [default: auto-detect]\n"
            "-nx                : start at beginning without looking for a start address\n"
            "-v                 : verbose\n"
@@ -316,7 +387,8 @@ err_usage:
            "\n"
            "Either -d or -l is required; -t is required.\n"
            "\n"
-           "For profiles, generate tracefile with 'trace log.run,maincpu,noloop'\n",
+           "Generate trace files in MAME debugger using:\n"
+           "  trace program.tr,maincpu,noloop,{tracelog \"A=%%02X,X=%%02X,Y=%%02X \",a,x,y}\n\n",
            argv[0]);
     exit(1);
   }
