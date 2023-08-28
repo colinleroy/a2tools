ec972c50bbbb05b4a7677ada23057e3847e7f96d
Colin Leroy-Mira <colin@colino.net>
2023-08-28 20:39:14 +0200

    Avoid a useless jsr

--- src/lib/simple_serial.h
+++ src/lib/simple_serial.h
@@ -18,10 +18,12 @@
 int __fastcall__ simple_serial_open(int slot, int baudrate);
 int __fastcall__ simple_serial_close(void);
 void __fastcall__ simple_serial_flush(void);
+#define simple_serial_putc(c) ser_put(c)
 #else
 int simple_serial_open(void);
 int simple_serial_close(void);
 void simple_serial_flush(void);
+unsigned char __fastcall__ simple_serial_putc(char c);
 #endif
 
 /* Input */
@@ -34,7 +36,6 @@ char * __fastcall__ simple_serial_gets(char *out, size_t size);
 void __fastcall__ simple_serial_read(char *ptr, size_t nmemb);
 
 /* Output */
-int __fastcall__ simple_serial_putc(char c);
 void __fastcall__ simple_serial_puts(char *buf);
 void __fastcall__ simple_serial_write(char *ptr, size_t nmemb);
 
