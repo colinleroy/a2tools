5b9f04dd8b75785a6af9a7899055e39b1b856611
Colin Leroy-Mira <colin@colino.net>
2023-09-15 19:15:38 +0200

    Refactor a bit the ugly, unloved one of this repo

--- src/surl-server/helpers/char-convert.h
+++ src/surl-server/helpers/char-convert.h
@@ -1,7 +1,7 @@
 #ifndef __char_convert_h
 #define __char_convert_h
 
-char *do_apple_convert(char *in, int way, char *a2charset, size_t *new_len);
+char *do_charset_convert(char *in, int way, char *a2charset, size_t *new_len);
 
 enum {
   INCOMING,
