b1fb888bd4a9da1fc974786dc29de91e1f21eae5
Colin Leroy-Mira <colin@colino.net>
2023-09-27 14:50:04 +0200

    Optimize str*split size

--- src/lib/strsplit.h
+++ src/lib/strsplit.h
@@ -6,11 +6,13 @@
 #endif
 
 /* Allocates out */
-int __fastcall__ strsplit(char *in, char split, char ***out);
-int __fastcall__ strsplit_in_place(char *in, char split, char ***out);
+int __fastcall__ _strsplit_int(char in_place, char *in, char split, char ***out);
+#define strsplit(in, split, out) _strsplit_int(0, in, split, out)
+#define strsplit_in_place(in, split, out) _strsplit_int(1, in, split, out)
 
 /* Does not allocate out! */
-int __fastcall__ strnsplit(char *in, char split, char **out, size_t max_tokens);
-int __fastcall__ strnsplit_in_place(char *in, char split, char **out, size_t max_tokens);
+int __fastcall__ _strnsplit_int(char in_place, char *in, char split, char **tokens, size_t max_tokens);
+#define strnsplit(in, split, out, max_tokens) _strnsplit_int(0, in, split, out, max_tokens)
+#define strnsplit_in_place(in, split, out, max_tokens) _strnsplit_int(1, in, split, out, max_tokens)
 
 #endif
