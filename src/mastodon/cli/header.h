2d7fb2d957fb5e65c3ea803cf91241bd40bc11fd
Colin Leroy-Mira <colin@colino.net>
2023-10-01 10:51:40 +0200

    Add a help toggle on 40cols

--- src/mastodon/cli/header.h
+++ src/mastodon/cli/header.h
@@ -4,6 +4,7 @@
 #include "list.h"
 
 char __fastcall__ print_header(list *l, status *root_status, notification *root_notif);
+void show_help (list *l, status *root_status, notification *root_notif);
 void __fastcall__ print_free_ram(void);
 
 #endif
