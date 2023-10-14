58262c4a234b992710cba3ee8a98c537d5cc7d22
Colin Leroy-Mira <colin@colino.net>
2023-10-14 14:21:56 +0200

    -547 bytes

--- src/mastodon/api/poll.h
+++ src/mastodon/api/poll.h
@@ -6,24 +6,22 @@ typedef struct _poll_option poll_option;
 
 #define MAX_POLL_OPTIONS 4
 
-struct _poll {
-  char *id;
-  char expired;
-  char multiple;
-  size_t votes_count;
-  size_t voters_count;
-  char options_count;
-  poll_option **options;
-};
-
 struct _poll_option {
   char *title;
   size_t votes_count;
 };
 
+struct _poll {
+  char *id;
+  char multiple;
+  size_t votes_count;
+  char options_count;
+  poll_option options[MAX_POLL_OPTIONS];
+};
+
 poll *poll_new(void);
 void poll_free(poll *p);
 
-void poll_fill(poll *p, const char *poll_sel);
+void poll_fill(poll *p, const char from_reblog);
 
 #endif
