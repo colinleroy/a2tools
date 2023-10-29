a4586a58f4f21dec9e6a9fea01532d28719f38e5
Colin Leroy-Mira <colin@colino.net>
2023-10-29 09:22:32 +0100

    Rename files to avoid clash with lib/hgr.h

--- src/surl-server/helpers/hgr-convert.h
+++ src/surl-server/helpers/hgr-convert.h
@@ -1,5 +1,5 @@
-#ifndef __hgr_h
-#define __hgr_h
+#ifndef __hgr_convert_h
+#define __hgr_convert_h
 
 char *hgr_to_png(char *hgr_data, size_t hgr_len, char monochrome, size_t *len);
 unsigned char *sdl_to_hgr(const char *filename, char monochrome, char save_preview, size_t *len);
