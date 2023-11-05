82aee4cbd97990f4691c934c6dc537f54be9ecc7
Colin Leroy-Mira <colin@colino.net>
2023-11-05 14:50:14 +0100

    More documentation

--- src/quicktake/qt1x0-serial.h
+++ src/quicktake/qt1x0-serial.h
@@ -1,16 +1,18 @@
 #ifndef __qt_1x0_serial_h
 #define __qt_1x0_serial_h
 
+/* Connection functions */
 uint8 qt_1x0_wakeup(uint16 speed);
-
 uint8 qt1x0_set_speed(uint16 speed);
 
+/* Camera settings functions */
 void qt1x0_set_camera_name(const char *name);
 void qt1x0_set_camera_time(uint8 day, uint8 month, uint8 year, uint8 hour, uint8 minute, uint8 second);
+uint8 qt1x0_get_information(uint8 *num_pics, uint8 *left_pics, uint8 *quality_mode, uint8 *flash_mode, uint8 *battery_level, char **name, struct tm *time);
 
+/* Camera pictures functions */
 uint8 qt1x0_take_picture(void);
 uint8 qt1x0_get_picture(uint8 n_pic, const char *filename, uint8 full);
 uint8 qt1x0_delete_pictures(void);
-uint8 qt1x0_get_information(uint8 *num_pics, uint8 *left_pics, uint8 *quality_mode, uint8 *flash_mode, uint8 *battery_level, char **name, struct tm *time);
 
 #endif
