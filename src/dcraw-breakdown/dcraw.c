eb1b98cee44d5e1a7d749b0ccc06571e5f28c6cb
Colin Leroy-Mira <colin@colino.net>
2023-12-16 22:50:32 +0100

    DCRAW: decode by bands

--- src/dcraw-breakdown/dcraw.c
+++ src/dcraw-breakdown/dcraw.c
@@ -5,9 +5,10 @@
 
 #define WIDTH 640
 #define HEIGHT 480
-uint8 raw_image[WIDTH*HEIGHT];
+#define BAND_HEIGHT 20
+uint8 raw_image[WIDTH*BAND_HEIGHT];
 
-FILE *fp;
+FILE *ifp, *ofp;
 
 #define RAW(row,col) \
         raw_image[(row)*WIDTH+(col)]
@@ -28,8 +29,8 @@ unsigned getbithuff (int nbits, uint16 *huff)
   if (nbits < 0)
     return bitbuf = vbits = reset = 0;
   if (nbits == 0 || vbits < 0) return 0;
-  while (!reset && vbits < nbits && (c = fgetc(fp)) != EOF &&
-    !(c == 0xff && fgetc(fp))) {
+  while (!reset && vbits < nbits && (c = fgetc(ifp)) != EOF &&
+    !(c == 0xff && fgetc(ifp))) {
     bitbuf = (bitbuf << 8) + (uint8) c;
     vbits += 8;
   }
@@ -45,7 +46,7 @@ unsigned getbithuff (int nbits, uint16 *huff)
 
 void quicktake_100_load_raw()
 {
-  uint8 pixel[HEIGHT+4][WIDTH+4];
+  uint8 pixel[BAND_HEIGHT+4][WIDTH+4];
   static const short gstep[16] =
   { -89,-60,-44,-32,-22,-15,-8,-2,2,8,15,22,32,44,60,89 };
   static const short rstep[6][4] =
@@ -69,7 +70,7 @@ void quicktake_100_load_raw()
   int first_line = 1, cnt;
   getbits(-1);
   memset (pixel, 0x80, sizeof pixel);
-  for (row=2; row < HEIGHT+2; row++) {
+  for (row=2; row < BAND_HEIGHT+2; row++) {
     int first_col = 1;
     cnt = 0;
     for (col=2+(row & 1); col < WIDTH+2; col+=2) {
@@ -87,7 +88,7 @@ void quicktake_100_load_raw()
     first_line=0;
   }
   for (rb=0; rb < 2; rb++)
-    for (row=2+rb; row < HEIGHT+2; row+=2)
+    for (row=2+rb; row < BAND_HEIGHT+2; row+=2)
       for (col=3-(row & 1); col < WIDTH+2; col+=2) {
         if (row < 4 || col < 4) sharp = 2;
         else {
@@ -103,26 +104,28 @@ void quicktake_100_load_raw()
         if (row < 4) pixel[row-2][col+2] = val;
         if (col < 4) pixel[row+2][col-2] = val;
       }
-  for (row=2; row < HEIGHT+2; row++)
+  for (row=2; row < BAND_HEIGHT+2; row++)
     for (col=3-(row & 1); col < WIDTH+2; col+=2) {
       val = ((pixel[row][col-1] + (pixel[row][col] << 2) +
               pixel[row][col+1]) >> 1) - 0x100;
       pixel[row][col] = LIM(val,0,255);
     }
-   for (row=0; row < HEIGHT; row++)
+   for (row=0; row < BAND_HEIGHT; row++)
      for (col=0; col < WIDTH; col++)
        RAW(row,col) = pixel[row+2][col+2];
 }
 
 int main(int argc, char *argv[]) {
-  fp = fopen("test.qtk", "r");
+  uint16 h;
+  ifp = fopen("test.qtk", "r");
+  ofp = fopen("out.raw","w");
   
-  fseek(fp, 736, SEEK_SET);
+  fseek(ifp, 736, SEEK_SET);
 
-  quicktake_100_load_raw();
-  fclose(fp);
-
-  fp = fopen("out.raw","w");
-  fwrite(raw_image, 1, 640*480, fp);
-  fclose(fp);
+  for (h = 0; h < HEIGHT; h+=BAND_HEIGHT) {
+    quicktake_100_load_raw();
+    fwrite(raw_image, 1, WIDTH*BAND_HEIGHT, ofp);
+  }
+  fclose(ifp);
+  fclose(ofp);
 }
