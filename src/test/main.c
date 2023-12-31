db9eb2aa9966e5f5c2fe23d5f6fc4ba9a7576ecf
Colin Leroy-Mira <colin@colino.net>
2023-12-31 11:14:25 +0100

    stat/statvfs/etc tests

--- src/test/main.c
+++ src/test/main.c
@@ -6,17 +6,34 @@
 #include <errno.h>
 #include <conio.h>
 #include <unistd.h>
-#include "get_filedetails.h"
-#include "surl.h"
-
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <sys/statvfs.h>
 int main(int argc, char *argv[]) {
-  unsigned char t;
-  unsigned st;
-  unsigned long size;
-  char *filename;
+  struct stat st;
+  struct statvfs sv;
+  int r;
+  printf("argc %d, argv1 %s\n", argc, argc > 1 ? argv[1]:"NULL");
+  r = stat("/RAM", &st);
+  printf("/RAM: %d (%d), %lu\n", r, errno, st.st_size);
+  r = stat("/qsdf", &st);
+  printf("/qsdf: %d (%d), %lu\n", r, errno, st.st_size);
+  r = stat("PRODOS", &st);
+  printf("PRODOS: %d (%d), %lu\n", r, errno, st.st_size);
   
-  surl_connect_proxy();
-  get_filedetails("PRODOS", &filename, &size, &t, &st);
-  printf("done. filename %s type %d size %zuB st %d\n", filename, size, t, st);
+  r = statvfs("/RAM", &sv);
+  printf("/RAM: %d(%d), %lu/%lu\n", r, errno, sv.f_bfree, sv.f_blocks);
+  r = statvfs("/qsdf", &sv);
+  printf("/qsdf: %d(%d), %lu/%lu\n", r, errno, sv.f_bfree, sv.f_blocks);
+  r = statvfs("/ADTPRO.2.1.0/PRODOS", &sv);
+  printf("/ADTPRO.2.1.0/PRODOS: %d(%d), %lu/%lu\n", r, errno, sv.f_bfree, sv.f_blocks);
+  r = statvfs("/RAM", &sv);
+  printf("/RAM: %d(%d), %lu/%lu\n", r, errno, sv.f_bfree, sv.f_blocks);
+  cgetc();
+  r = exec("/prout/test", "path");
+  printf("exec: %d (%d)\n", r, errno);
+  cgetc();
+  r = exec("/ADTPRO.2.1.0/test", "path");
+  printf("exec: %d (%d)\n", r, errno);
   cgetc();
 }
