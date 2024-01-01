a30ce1b4d6a1914f1ec1b0ec21fa74cbbd8f93f9
Colin Leroy-Mira <colin@colino.net>
2024-01-01 20:14:47 +0100

    Get rid of get_filedetails and use stat() instead

--- src/lib/get_filedetails.h
+++
@@ -1,6 +0,0 @@
-#ifndef __get_filedetails_h
-#define __get_filedetails_h
-
-int __fastcall__ get_filedetails(char *path, char **filename, unsigned long *size, unsigned char *type, unsigned *auxtype);
-
-#endif
