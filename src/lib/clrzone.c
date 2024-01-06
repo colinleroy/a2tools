79c8bfa0949f1b5fa4a219605039ac6ba83ef945
Colin Leroy-Mira <colin@colino.net>
2024-01-06 14:12:34 +0100

    A way to spend less time in ROM clearing screen

--- src/lib/clrzone.c
+++ src/lib/clrzone.c
@@ -27,49 +27,85 @@ static char clearbuf[82];
 void __fastcall__ clrzone(char xs, char ys, char xe, char ye) {
 #ifdef __APPLE2__
   /* Load by stack order */
-  unsigned char lxs = xs;
+  unsigned char lxs_even = xs;
   unsigned char lys = ys;
-  unsigned char new_wdth = xe - lxs + 1;
+  unsigned char lxe_even = xe;
   unsigned char lye = ye;
+  unsigned char lxs_odd, lxe_odd;
 
-  /* Backup and update scrollwindow boundaries */
-  __asm__("lda "WNDBTM);
-  __asm__("pha");
+  /* Add WNDTOP */
+  __asm__("clc");
   __asm__("lda "WNDTOP);
-  __asm__("pha");
-  __asm__("clc");
   __asm__("adc %v", lys);
-  __asm__("sta "WNDTOP);
-  __asm__("pla");
+  __asm__("sta %v", lys);
+  /* Save start Y */
   __asm__("pha");
-  __asm__("clc");
+
+  __asm__("lda "WNDTOP);
   __asm__("adc %v", lye);
   __asm__("adc #1");
-  __asm__("sta "WNDBTM);
+  __asm__("sta %v", lye);
 
-  __asm__("lda "WNDLFT);
+  /* Fix even/odd boundaries */
+  __asm__("lda %v", lxs_even);
   __asm__("pha");
+  __asm__("lsr");
+  __asm__("sta %v", lxs_odd);
+  __asm__("bcc %g", lxs_is_even);
+  __asm__("adc #0");
+lxs_is_even:
+  __asm__("sta %v", lxs_even);
+
   __asm__("clc");
-  __asm__("adc %v", lxs);
-  __asm__("sta "WNDLFT);
-  __asm__("lda "WNDWDTH);
-  __asm__("pha");
-  __asm__("lda %v", new_wdth);
-  __asm__("sta "WNDWDTH);
+  __asm__("lda %v", lxe_even);
+  __asm__("adc #1");
+  __asm__("lsr");
+  __asm__("sta %v", lxe_odd);
+  __asm__("bcc %g", lxe_is_even);
+  __asm__("adc #0");
+lxe_is_even:
+  __asm__("sta %v", lxe_even);
 
-  clrscr();
-  
-  /* Restore boundaries */
-  __asm__("pla");
-  __asm__("sta "WNDWDTH);
-  __asm__("pla");
-  __asm__("sta "WNDLFT);
-  __asm__("pla");
-  __asm__("sta "WNDTOP);
-  __asm__("pla");
-  __asm__("sta "WNDBTM);
+  /* Start clearing */
+  __asm__("lda %v", lys);
+  __asm__("sta "CV);
 
-  gotoxy(lxs, lys);
+next_line:
+  __asm__("jsr VTABZ");
+  __asm__("lda #' '|$80");
+
+  __asm__("bit $C055");
+  __asm__("ldy %v", lxs_even);
+next_char_hi:
+  __asm__("cpy %v", lxe_even);
+  __asm__("bcs %g", do_low);
+  __asm__("sta ($28),y");
+  __asm__("iny");
+  __asm__("bne %g", next_char_hi);
+
+do_low:
+  __asm__("bit $C054");
+  __asm__("ldy %v", lxs_odd);
+next_char_low:
+  __asm__("cpy %v", lxe_odd);
+  __asm__("bcs %g", do_next_line);
+  __asm__("sta ($28),y");
+  __asm__("iny");
+  __asm__("bne %g", next_char_low);
+
+do_next_line:
+  __asm__("inc "CV);
+  __asm__("lda "CV);
+  __asm__("cmp %v", lye);
+  __asm__("bcc %g", next_line);
+
+  /* Goto start of cleared zone */
+  __asm__("pla");
+  __asm__("sta "CH);
+  __asm__("pla");
+  __asm__("sta "CV);
+  __asm__("jsr VTABZ");
+
 #else
   char l = xe - xs + 1;
 
