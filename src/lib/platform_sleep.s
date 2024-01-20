c83e00a48ce010fe64101acf2886e7409263ed6b
Colin Leroy-Mira <colin@colino.net>
2024-01-20 16:17:28 +0100

    Integrate sleep() from cc65

--- src/lib/platform_sleep.s
+++ src/lib/platform_sleep.s
@@ -2,57 +2,40 @@
 ; Colin Leroy-Mira <colin@colino.net>, 2023
 ;
 
-        .export         _platform_sleep
         .export         _platform_msleep
-        .import         _slowdown
-        .import         _speedup
+        .import         _get_iigs_speed, _set_iigs_speed
         .importzp       tmp1
 
-;void __fastcall__ platform_sleep(uint16 s)
-
-_platform_sleep:
-        stx     tmp1
-        tay
-        ora     tmp1
-        beq     out
-        jsr     _slowdown; Down to 1MHz for consistency around MONWAIT
-        bit     $C082
-sleep_1s:
-        ldx     #10             ; Loop 10 times
-sleep_100ms:
-        lda     #199            ; Sleep about 99ms
-        jsr     $FCA8
-        lda     #12             ; About 1ms
-        jsr     $FCA8
-        dex
-        bne     sleep_100ms
-        dey
-        bne     sleep_1s
-        dec     tmp1
-        bmi     done
-        ldy     #$FF
-        bne     sleep_1s
-done:
-        bit     $C080
-        jsr     _speedup
-out:
-        rts
+        .include        "accelerator.inc"
 
 ;void __fastcall__ platform_msleep(uint16 ms)
 
 _platform_msleep:
-        tay                     ; low byte in Y, high byte in X
-        jsr     _slowdown
+        pha                     ; Backup AX
+        txa
+        pha
+        jsr     _get_iigs_speed
+        sta     prevspd
+        lda     #SPEED_SLOW
+        jsr     _set_iigs_speed
         bit     $C082
 
-:       lda     #$11
+        pla                     ; Restore AX
+        tax
+        pla
+        tay                     ; Move low byte to Y
+
+:       lda     #$0F
         jsr     $FCA8
-        lda     #$2
+        lda     #$03
         jsr     $FCA8
         dey
         bne     :-
         dex
         bpl     :-
         bit     $C080
-        jsr     _speedup
-        rts
+        lda     prevspd
+        jmp     _set_iigs_speed
+
+        .bss
+prevspd:.res 1
