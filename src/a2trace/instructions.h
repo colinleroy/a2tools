1613ed797cafe1f3bdf417fd29f10325f7067297
Colin Leroy-Mira <colin@colino.net>
2024-01-21 20:49:03 +0100

    65816 fixes

--- src/a2trace/instructions.h
+++ src/a2trace/instructions.h
@@ -36,7 +36,7 @@ int update_call_counters(int cpu, int op_addr, const char *instr, int param_addr
 int is_instruction_write(const char *instr);
 int analyze_instruction(int cpu, int op_addr, const char *instr, int param_addr, char *comment);
 
-int instruction_get_addressing_mode(int cpu, const char *arg);
+int instruction_get_addressing_mode(int cpu, const char *instr, const char *arg);
 int get_cycles_for_instr(int cpu, const char *instr, int a_mode, int *extra_cost_if_taken);
 
 /* Memory banking */
