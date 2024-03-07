0d095f57ae79e79b680f4f127795452b0249dfb6
Colin Leroy-Mira <colin@colino.net>
2024-03-07 21:46:44 +0100

    Split FTP listing / UI a bit more

--- src/stp/stp.h
+++ src/stp/stp.h
@@ -15,8 +15,32 @@
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
+#ifndef __stp_h__
+#define __stp_h__
+
 #define BUFSIZE 255
 #define STP_URL_FILE "STPSTARTURL"
 
 #define PAGE_BEGIN 2
 #define PAGE_HEIGHT 17
+
+enum StpDataAction {
+  KEYBOARD_INPUT,
+  URL_UP,
+  UPDATE_LIST
+};
+
+extern char **lines;
+extern int num_lines;
+extern int cur_line;
+extern int cur_display_line;
+extern char *data;
+char *stp_url_enter(char *url, char *suffix);
+char *stp_url_up(char *url);
+char stp_list_scroll(signed char shift);
+char *stp_build_login_url(char *url);
+char *stp_get_start_url(void);
+void stp_update_list(char full_update);
+int stp_get_data(char *url);
+
+#endif
