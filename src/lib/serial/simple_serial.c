94a40a96799de98fee36441aa18fc7f098253321
Colin Leroy-Mira <colin@colino.net>
2024-04-11 22:43:07 +0200

    Rename av_tty to aux_tty

--- src/lib/serial/simple_serial.c
+++ src/lib/serial/simple_serial.c
@@ -210,7 +210,7 @@ extern char *readbuf;
 int bps = B19200;
 
 static char *opt_tty_path = NULL;
-char *av_tty_path = NULL;
+char *aux_tty_path = NULL;
 
 static int opt_tty_speed = B115200;
 static int opt_tty_hw_handshake = 1;
@@ -292,13 +292,13 @@ static void simple_serial_write_defaults(void) {
   fprintf(fp, "tty: /dev/ttyUSB0\n"
               "baudrate: 115200\n"
               "hw_handshake: off\n"
-              "av_tty: /dev/ttyUSB1\n"
+              "aux_tty: /dev/ttyUSB1\n"
               "\n"
               "#Alternatively, you can export environment vars:\n"
               "#A2_TTY (unset by default)\n"
               "#A2_TTY_SPEED (default %s)\n"
               "#A2_TTY_HW_HANDSHAKE (default %s)\n"
-              "#A2_AV_TTY (unset by default)\n",
+              "#A2_AUX_TTY (unset by default)\n",
               tty_speed_to_str(opt_tty_speed),
               opt_tty_hw_handshake ? "true":"false");
   if (fp == stdout) {
@@ -324,9 +324,9 @@ static int simple_serial_read_opts(void) {
       opt_tty_path = trim(buf + 4);
     }
 
-    if(!strncmp(buf, "av_tty:", 7)) {
-      free(av_tty_path);
-      av_tty_path = trim(buf + 7);
+    if(!strncmp(buf, "aux_tty:", 8)) {
+      free(aux_tty_path);
+      aux_tty_path = trim(buf + 8);
     }
 
     if (!strncmp(buf,"baudrate:", 9)) {
@@ -351,9 +351,9 @@ static int simple_serial_read_opts(void) {
     opt_tty_path = strdup(getenv("A2_TTY"));
   }
 
-  if (getenv("A2_AV_TTY")) {
-    free(av_tty_path);
-    av_tty_path = strdup(getenv("A2_AV_TTY"));
+  if (getenv("A2_AUX_TTY")) {
+    free(aux_tty_path);
+    aux_tty_path = strdup(getenv("A2_AUX_TTY"));
   }
 
   if (getenv("A2_TTY_SPEED")) {
