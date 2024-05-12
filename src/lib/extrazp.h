d55591d93e79b7d7039c6bbe2360524508dc61fe
Colin Leroy-Mira <colin@colino.net>
2024-05-12 17:31:33 +0200

    Little round of dithering optimisation

--- src/lib/extrazp.h
+++ src/lib/extrazp.h
@@ -12,6 +12,10 @@ extern uint8 *prev_ram_irq_vector;
 extern uint8 *prev_rom_irq_vector;
 extern uint8 a_backup;
 
+extern uint8 zp2;
+extern uint8 zp3;
+extern uint8 zp4;
+extern uint8 zp5;
 extern uint8 zp6;
 extern uint8 zp7;
 extern uint8 zp8;
@@ -21,6 +25,10 @@ extern uint8 zp11;
 extern uint8 zp12;
 extern uint8 zp13;
 
+extern int8 zp2s;
+extern int8 zp3s;
+extern int8 zp4s;
+extern int8 zp5s;
 extern int8 zp6s;
 extern int8 zp7s;
 extern int8 zp8s;
@@ -30,31 +38,43 @@ extern int8 zp11s;
 extern int8 zp12s;
 extern int8 zp13s;
 
+extern uint8 *zp2p;
+extern uint8 *zp4p;
 extern uint8 *zp6p;
 extern uint8 *zp8p;
 extern uint8 *zp10p;
 extern uint8 *zp12p;
 
+extern int8 *zp2sp;
+extern int8 *zp4sp;
 extern int8 *zp6sp;
 extern int8 *zp8sp;
 extern int8 *zp10sp;
 extern int8 *zp12sp;
 
+extern uint16 *zp2ip;
+extern uint16 *zp4ip;
 extern uint16 *zp6ip;
 extern uint16 *zp8ip;
 extern uint16 *zp10ip;
 extern uint16 *zp12ip;
 
+extern uint16 zp2i;
+extern uint16 zp4i;
 extern uint16 zp6i;
 extern uint16 zp8i;
 extern uint16 zp10i;
 extern uint16 zp12i;
 
+extern int16 zp2si;
+extern int16 zp4si;
 extern int16 zp6si;
 extern int16 zp8si;
 extern int16 zp10si;
 extern int16 zp12si;
 
+extern int16 *zp2sip;
+extern int16 *zp4sip;
 extern int16 *zp6sip;
 extern int16 *zp8sip;
 extern int16 *zp10sip;
@@ -66,6 +86,10 @@ extern int16 *zp12sip;
 #pragma zpsym ("prev_rom_irq_vector")
 #pragma zpsym ("a_backup")
 
+#pragma zpsym ("zp2")
+#pragma zpsym ("zp3")
+#pragma zpsym ("zp4")
+#pragma zpsym ("zp5")
 #pragma zpsym ("zp6")
 #pragma zpsym ("zp7")
 #pragma zpsym ("zp8")
@@ -75,31 +99,43 @@ extern int16 *zp12sip;
 #pragma zpsym ("zp12")
 #pragma zpsym ("zp13")
 
+#pragma zpsym ("zp2p")
+#pragma zpsym ("zp4p")
 #pragma zpsym ("zp6p")
 #pragma zpsym ("zp8p")
 #pragma zpsym ("zp10p")
 #pragma zpsym ("zp12p")
 
+#pragma zpsym ("zp2sp")
+#pragma zpsym ("zp4sp")
 #pragma zpsym ("zp6sp")
 #pragma zpsym ("zp8sp")
 #pragma zpsym ("zp10sp")
 #pragma zpsym ("zp12sp")
 
+#pragma zpsym ("zp2ip")
+#pragma zpsym ("zp4ip")
 #pragma zpsym ("zp6ip")
 #pragma zpsym ("zp8ip")
 #pragma zpsym ("zp10ip")
 #pragma zpsym ("zp12ip")
 
+#pragma zpsym ("zp2i")
+#pragma zpsym ("zp4i")
 #pragma zpsym ("zp6i")
 #pragma zpsym ("zp8i")
 #pragma zpsym ("zp10i")
 #pragma zpsym ("zp12i")
 
+#pragma zpsym ("zp2si")
+#pragma zpsym ("zp4si")
 #pragma zpsym ("zp6si")
 #pragma zpsym ("zp8si")
 #pragma zpsym ("zp10si")
 #pragma zpsym ("zp12si")
 
+#pragma zpsym ("zp2sip")
+#pragma zpsym ("zp4sip")
 #pragma zpsym ("zp6sip")
 #pragma zpsym ("zp8sip")
 #pragma zpsym ("zp10sip")
--- src/lib/extrazp.s
+++ src/lib/extrazp.s
@@ -3,14 +3,14 @@
 ; Colin Leroy-Mira <colin@colino.net>, 2023
 ;
         .export         _a_backup, _prev_ram_irq_vector, _prev_rom_irq_vector
-        .export         _zp6, _zp7, _zp8, _zp9, _zp10, _zp11, _zp12, _zp13
-        .export         _zp6s, _zp7s, _zp8s, _zp9s, _zp10s, _zp11s, _zp12s, _zp13s
-        .export         _zp6p, _zp8p, _zp10p, _zp12p
-        .export         _zp6ip, _zp8ip, _zp10ip, _zp12ip
-        .export         _zp6sp, _zp8sp, _zp10sp, _zp12sp
-        .export         _zp6sip, _zp8sip, _zp10sip, _zp12sip
-        .export         _zp6i, _zp8i, _zp10i, _zp12i
-        .export         _zp6si, _zp8si, _zp10si, _zp12si
+        .export         _zp2, _zp3, _zp4, _zp5, _zp6, _zp7, _zp8, _zp9, _zp10, _zp11, _zp12, _zp13
+        .export         _zp2s, _zp3s, _zp4s, _zp5s, _zp6s, _zp7s, _zp8s, _zp9s, _zp10s, _zp11s, _zp12s, _zp13s
+        .export         _zp2p, _zp4p, _zp6p, _zp8p, _zp10p, _zp12p
+        .export         _zp2ip, _zp4ip, _zp6ip, _zp8ip, _zp10ip, _zp12ip
+        .export         _zp2sp, _zp4sp, _zp6sp, _zp8sp, _zp10sp, _zp12sp
+        .export         _zp2sip, _zp4sip, _zp6sip, _zp8sip, _zp10sip, _zp12sip
+        .export         _zp2i, _zp4i, _zp6i, _zp8i, _zp10i, _zp12i
+        .export         _zp2si, _zp4si, _zp6si, _zp8si, _zp10si, _zp12si
 
 ; https://fadden.com/apple2/dl/zero-page.txt
 ; Of course don't use them badly
@@ -19,6 +19,28 @@
 _prev_ram_irq_vector  := $EB
 _prev_rom_irq_vector  := $ED
 _a_backup             := $EF
+
+_zp2  := $50
+_zp2i := $50
+_zp2s := $50
+_zp2si:= $50
+_zp2sp:= $50
+_zp2p := $50
+_zp2ip:= $50
+_zp2sip:= $50
+_zp3  := $51
+_zp3s := $51
+
+_zp4  := $52
+_zp4i := $52
+_zp4s := $52
+_zp4si:= $52
+_zp4sp:= $52
+_zp4p := $52
+_zp4ip:= $52
+_zp4sip:= $52
+_zp5  := $53
+_zp5s := $53
 
 _zp6  := $54
 _zp6i := $54
