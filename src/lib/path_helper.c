d0e52fe78d929697b61171a6d9184201912296d7
Colin Leroy-Mira <colin@colino.net>
2024-06-29 17:19:09 +0200

    Fix need to start program in correct directory
    Helper programs exec()ed should now be found better
    in the current program's start directory.

--- src/lib/path_helper.c
+++ src/lib/path_helper.c
@@ -21,11 +21,45 @@
 #include <string.h>
 #include <unistd.h>
 #include <errno.h>
+#include <conio.h>
 
-static char start_dir[FILENAME_MAX];
+static char start_dir[FILENAME_MAX*2];
 
 void register_start_device(void) {
+#ifdef __APPLE2__
+  static char argv0_len;
+  char *start_dir_end = start_dir;
+
+  /* If current program pathname is absolute,
+   * skip prefix */
+  __asm__("lda $281");
+  __asm__("cmp #%b", '/');
+  __asm__("beq %g", skip_prefix);
+
+  /* Get current PREFIX */
   getcwd(start_dir, FILENAME_MAX);
+  start_dir_end = start_dir+strlen(start_dir);
+  /* Add a slash at end of directory, to concatenate
+   * a possibly-relative filename to it */
+  if (*start_dir_end != '/') {
+    *start_dir_end = '/';
+    start_dir_end++;
+  }
+
+skip_prefix:
+  /* Get the path name to the program */
+  __asm__("lda $280"); /* Get its length */
+  __asm__("sta %v", argv0_len);
+
+  /* Copy it */
+  memcpy(start_dir_end, (char *)0x281, argv0_len);
+  start_dir_end[argv0_len] = '\0';
+
+  /* Cut the filename off */
+  *strrchr(start_dir, '/') = '\0';
+#else
+  getcwd(start_dir, FILENAME_MAX);
+#endif
 }
 
 int reopen_start_device(void) {
