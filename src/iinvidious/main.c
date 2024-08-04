ff80b2d5d8d37a3294588ea666ab5c375aec8340
Colin Leroy-Mira <colin@colino.net>
2024-08-04 13:44:09 +0200

    Basis of UI

--- src/iinvidious/main.c
+++ src/iinvidious/main.c
@@ -24,6 +24,7 @@
 #include "config.h"
 #include "surl.h"
 #include "simple_serial.h"
+#include "strsplit.h"
 #include "extended_conio.h"
 #include "dgets.h"
 #include "dputc.h"
@@ -41,11 +42,11 @@
 #include "malloc0.h"
 #include "videoplay.h"
 
-char *data = (char *)splash_hgr;
+char *url = NULL;
 
 unsigned char scrw = 255, scrh = 255;
 
-extern char tmp_buf[80];
+char search_str[80];
 
 #pragma code-name(push, "LOWCODE")
 
@@ -62,21 +63,104 @@ static void backup_restore_logo(char *op) {
   fclose(fp);
 }
 
-static void load_video(char *url, char *id) {
-  sprintf((char *)BUF_ADDR, "%s/api/v1/videos/%s", url, id);
+static void load_video(char *id) {
+  sprintf((char *)BUF_1K_ADDR, "%s/api/v1/videos/%s?local=true", url, id);
 
-  surl_start_request(SURL_METHOD_GET, (char *)BUF_ADDR, NULL, 0);
+  surl_start_request(SURL_METHOD_GET, (char *)BUF_1K_ADDR, NULL, 0);
 
   if (!surl_response_ok()) {
+    clrscr();
+    gotoxy(0, 20);
+    printf("Error loading video: %d", surl_response_code());
+    cgetc();
     return;
   }
 
-  if (surl_get_json((char *)BUF_ADDR, BUF_SIZE, SURL_HTMLSTRIP_NONE, translit_charset,
+  if (surl_get_json((char *)BUF_1K_ADDR, BUF_1K_SIZE, SURL_HTMLSTRIP_NONE, translit_charset,
                     ".formatStreams[]|select(.itag==\"18\").url") >= 0) {
-    printf("URL: %s\nHit a key to play", (char *)BUF_ADDR);
-    cgetc();
-    stream_url((char *)BUF_ADDR);
+    stream_url((char *)BUF_1K_ADDR);
   }
+}
+
+char **lines;
+char n_lines;
+char cur_line = 0;
+
+static int search_results(void) {
+  int len;
+  char c;
+
+  n_lines = strsplit_in_place((char *)BUF_8K_ADDR, '\n', &lines);
+  if (n_lines % 4 != 0) {
+    cputs("Search error\n");
+    cgetc();
+    return -1;
+  }
+
+  init_hgr(1);
+  hgr_mixon();
+
+display_result:
+  clrscr();
+  gotoxy(0, 20);
+  printf("%s (%s)\nUploaded by %s\n\n%d/%d results", lines[cur_line], lines[cur_line+1], lines[cur_line+2],
+         (cur_line/4)+1, n_lines/4);
+
+  surl_start_request(SURL_METHOD_GET, lines[cur_line+3], NULL, 0);
+
+  if (surl_response_ok()) {
+
+    simple_serial_putc(SURL_CMD_HGR);
+    simple_serial_putc(1); /* monochrome */
+    simple_serial_putc(HGR_SCALE_FULL);
+
+    if (simple_serial_getc() == SURL_ERROR_OK) {
+
+      surl_read_with_barrier((char *)&len, 2);
+      len = ntohs(len);
+
+      if (len == HGR_LEN) {
+        surl_read_with_barrier((char *)HGR_PAGE, HGR_LEN);
+      }
+    }
+  }
+  c = cgetc();
+  switch (c) {
+    case CH_ENTER:
+      load_video(lines[cur_line+1]);
+      /* relaunch search */
+      return 0;
+    case CH_ESC:
+      init_text();
+      return -1;
+    case CH_CURS_LEFT:
+      if (cur_line > 3) {
+        cur_line -= 4;
+      }
+      break;
+    case CH_CURS_RIGHT:
+      if (cur_line + 4 < n_lines) {
+        cur_line += 4;
+      }
+      break;
+  }
+  goto display_result;
+}
+
+static int search(void) {
+  sprintf((char *)BUF_1K_ADDR, "%s/api/v1/search?type=video&q=%s", url, search_str);
+
+  surl_start_request(SURL_METHOD_GET, (char *)BUF_1K_ADDR, NULL, 0);
+
+  if (!surl_response_ok()) {
+    return -1;
+  }
+
+  if (surl_get_json((char *)BUF_8K_ADDR, BUF_8K_SIZE, SURL_HTMLSTRIP_NONE, translit_charset,
+                    ".[]|.title,.videoId,.author,(.videoThumbnails[]|select(.quality == \"medium\")|.url)") >= 0) {
+    return search_results();
+  }
+  return -1;
 }
 
 #ifdef __CC65__
@@ -85,8 +169,6 @@ static void load_video(char *url, char *id) {
 #endif
 
 static char *do_setup(void) {
-  char *url = NULL;
-
   clrscr();
   url = strdup("https://invidious.fdn.fr/");
 
@@ -119,11 +201,17 @@ int main(void) {
 
   url = do_setup();
   printf("started; %zuB free\n", _heapmaxavail());
-  cputs("Enter video ID: ");
-again:
-  dget_text(tmp_buf, 80, NULL, 0);
-  load_video(url, tmp_buf);
-  goto again;
+
+new_search:
+  clrscr();
+  cputs("Search: ");
+  dget_text(search_str, 80, NULL, 0);
+  cur_line = 0;
+same_search:
+  if (search() == 0) {
+    goto same_search;
+  }
+  goto new_search;
 }
 
 #ifdef __CC65__
