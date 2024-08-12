4ef804a39449ba4dbc7b67becf6d747a1c62160a
Colin Leroy-Mira <colin@colino.net>
2024-08-12 08:27:46 +0200

    Fix comments

--- src/lib/surl/surl_stream_av.s
+++ src/lib/surl/surl_stream_av.s
@@ -1745,7 +1745,7 @@ ad29b:  ldx     $A8FF           ; 21
 .align $100
 .assert * = _SAMPLES_BASE + $1E00, error
 ; Duty cycle 30 must toggle off speaker at cycle 38, but we would have to jump
-; to video_sub    at cycle 39, so this one uses different entry points to
+; to video_sub at cycle 39, so this one uses different entry points to
 ; the video handler to fix this.
 duty_cycle30:                    ; end spkr at 38
         ____SPKR_DUTY____4      ; 4
@@ -1784,29 +1784,29 @@ video_sub:
         beq     @toggle_page            ; 4/5   Result of cpy #$7F
         ldy     next_offset             ; 7     Load the offset to the start of the base
         sta     (cur_base),y            ; 13    Store data byte
-        inc     next_offset             ; 18    and store it.
+        inc     next_offset             ; 18    and increment offset.
         clv                             ; 20    Reset the offset-received flag.
         jmp     (next)                  ; 26    Done, go to next duty cycle
 
 @maybe_control:
         bvc     @set_offset             ; 5/6   If V flag is set, this one is a base byte
 
-@set_base:                              ;       This is a base byte (branch takes 22 cycles minimum)
+@set_base:                              ;       This is a base byte
         lda     pages_addrs_arr_low,y   ; 9     Load base pointer low byte from base array
         sta     cur_base                ; 12    Store it to destination pointer low byte
         lda     (page_ptr_high),y       ; 17    Load base pointer high byte from base array
         sta     cur_base+1              ; 20    Store it to destination pointer high byte
         jmp     (next)                  ; 26    Done, go to next duty cycle
 
-@set_offset:                            ;       No, so set offset or toggle page
+@set_offset:                            ;       This is an offset byte
         sty     next_offset             ; 9     Store offset
         lda     page_ptr_high+1         ; 12    Update the page flag here, where we have time
-        and     #$55                    ; 14    Use the fact that page1 array's high byte is odd
+        and     #$55                    ; 14    Use the fact that page1 array's high byte is odd, and page0's is even
         sta     @toggle_page+1          ; 18
         adc     #$30                    ; 20    $54/$55 + $30 => sets V flag
         jmp     (next)                  ; 26    Done, go to next duty cycle
 
-@toggle_page:                           ;       Page toggling command (branch takes 21 cycles minimum)
+@toggle_page:                           ;       Page toggling
 .ifdef DOUBLE_BUFFER
         lda     $C054                   ; 9     Activate page (patched by set_offset)
         lda     page_ptr_high+1         ; 12    Toggle written page
