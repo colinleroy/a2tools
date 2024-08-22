3e3872b46e9b268315d953ef3516e0c6756664c8
Colin Leroy-Mira <colin@colino.net>
2024-08-22 08:08:10 +0200

    Split realloc_safe

--- src/lib/realloc_safe.c
+++ src/lib/realloc_safe.c
@@ -2,13 +2,12 @@
 #include <string.h>
 #include "malloc0.h"
 
-void * __fastcall__ malloc0(size_t size) {
-  void *p = malloc(size);
+void *__fastcall__ realloc_safe(void *ptr, size_t size) {
+  void *p = realloc(ptr, size);
 #ifdef __CC65__
   if (!p && size) {
     __asm__("brk");
   }
 #endif
-  memset(p, 0, size);
   return p;
 }
