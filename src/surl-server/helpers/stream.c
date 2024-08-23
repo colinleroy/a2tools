3d997ca1b6ab96dc1fa35c05b808f707bbb6ddab
Colin Leroy-Mira <colin@colino.net>
2024-08-23 20:50:15 +0200

    Parametrize AV samples range

--- src/surl-server/helpers/stream.c
+++ src/surl-server/helpers/stream.c
@@ -404,11 +404,13 @@ static byte_diff **diffs = NULL;
 #define AUDIO_NUM_LEVELS         (AUDIO_MAX_LEVEL+1)
 #define AUDIO_END_OF_STREAM      AUDIO_NUM_LEVELS
 
-#define AV_SAMPLE_OFFSET         0x00
 #define AV_MAX_LEVEL             31
 #define AV_NUM_LEVELS            (AV_MAX_LEVEL+1)
 #define AV_END_OF_STREAM         AV_NUM_LEVELS
 #define AV_KBD_LOAD_LEVEL        16
+
+int av_sample_offset = 0;
+int av_sample_multiplier = 2;
 
 #define send_sample(i) fputc((i) + AUDIO_SAMPLE_OFFSET, ttyfp)
 
@@ -416,7 +418,7 @@ static int num_audio_samples = 0;
 static unsigned char audio_samples_buffer[SAMPLE_RATE*2];
 
 static inline void buffer_audio_sample(unsigned char i) {
-  audio_samples_buffer[num_audio_samples++] = (i + AV_SAMPLE_OFFSET)*2;
+  audio_samples_buffer[num_audio_samples++] = (i + av_sample_offset)*av_sample_multiplier;
 }
 
 static inline void flush_audio_samples(void) {
@@ -1561,6 +1563,10 @@ int surl_stream_audio_video(char *url, char *translit, char monochrome, char sub
     ret = -1;
     goto cleanup_thread;
   }
+
+  av_sample_offset = simple_serial_getc();
+  av_sample_multiplier = simple_serial_getc();
+  printf("Sample offset is %d mult %d\n", av_sample_offset, av_sample_multiplier);
 
   sleep(1); /* Let ffmpeg have a bit of time to push data so we don't starve */
 
