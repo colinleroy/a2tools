f818387ed7c5c50a47771618d6ff6bb0dce10e25
Colin Leroy-Mira <colin@colino.net>
2024-08-24 18:49:38 +0200

    Remove a useless level of indirection

--- src/lib/surl/surl_read_with_barrier.s
+++ src/lib/surl/surl_read_with_barrier.s
@@ -3,10 +3,11 @@
 
         .import         _serial_putc_direct
         .import         _simple_serial_set_irq
-        .import         _simple_serial_read_no_irq
 
-        .importzp       tmp1, tmp2
+        .import         popax
+        .importzp       ptr3, ptr4
         .export         _surl_read_with_barrier
+        .import         _serial_read_byte_no_irq
 
         .include        "apple2.inc"
         .include        "ser-kernel.inc"
@@ -19,18 +20,48 @@
         .endif
 
 _surl_read_with_barrier:
-        sta     tmp1
-        stx     tmp2
+        php
+        sei
+        sta     ptr3            ; Store nmemb
+        stx     ptr3+1
+
+        jsr     popax
+        sta     ptr4            ; Store buffer
+        stx     ptr4+1
+
+        ldy     #0              ; Inner loop counter
 
         lda     #0
         jsr     _simple_serial_set_irq
 
-        ldx     tmp2
-
         lda     #$2F            ; SURL_CLIENT_READY
         jsr     _serial_putc_direct
 
-        lda     tmp1
-        jsr     _simple_serial_read_no_irq
+        ldx     ptr3+1          ; Get number of full pages
+        beq     last_page       ; Only a partial one
+
+:       jsr     _serial_read_byte_no_irq
+        sta     (ptr4),y
+        iny
+        bne     :-              ; next byte
+
+        inc     ptr4+1
+        dex
+        bne     :-              ; next page
+
+last_page:
+        ldx     ptr3            ; Y safely 0 there
+        beq     done            ; Nothing to read
+
+:       jsr     _serial_read_byte_no_irq
+        sta     (ptr4),y
+        iny
+        dex
+        bne     :-
+
+done:
         lda     #1
-        jmp     _simple_serial_set_irq
+        jsr     _simple_serial_set_irq
+
+        plp
+        rts
