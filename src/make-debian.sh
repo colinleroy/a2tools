f88c6db3b059df17cceb4a28600da99077bf7d12
Colin Leroy-Mira <colin@colino.net>
2024-08-24 16:49:43 +0200

    Fix path to /etc when building locally
    Thanks to Fozztexx

--- src/make-debian.sh
+++ src/make-debian.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 DESTDIR=$1
 make clean
-make -C surl-server
-make -C a2send a2send.x86_64
-make -C a2recv a2recv.x86_64
+make -C surl-server prefix=/usr etc_prefix=/etc
+make -C a2send a2send.x86_64 prefix=/usr etc_prefix=/etc
+make -C a2recv a2recv.x86_64 prefix=/usr etc_prefix=/etc
 make -C a2trace a2trace
