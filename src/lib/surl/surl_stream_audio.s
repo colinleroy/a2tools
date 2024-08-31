042595cc5d02e304de2882ba19ac39821da1ae59
Colin Leroy-Mira <colin@colino.net>
2024-08-31 10:51:33 +0200

    Comment

--- src/lib/surl/surl_stream_audio.s
+++ src/lib/surl/surl_stream_audio.s
@@ -1444,39 +1444,46 @@ numcols:ldx     #0              ; Reset char counter
 
 st:     SER_AVAIL_A_6           ; Fetch a title character
         beq     st
-dt:     SER_FETCH_DEST_A_4
-        ora     #$80
+
+                                ; 6502  65C02 (cycles since fetch, worst case,
+                                ;              must be under 85 for serial access)
+dt:     SER_FETCH_DEST_A_4      ; 4     4
+        ora     #$80            ; 6     6
+
         .ifndef __APPLE2ENH__
-        cmp     #$E0            ; Should we uppercase?
-        bcc     :+
-        and     uppercasemask
+        cmp     #$E0            ; 8            Should we uppercase?
+        bcc     :+              ; 10/11
+        and     uppercasemask   ; 13
 :
         .endif
 
-        pha                     ; Local copy of putchardirect
-        .ifdef  __APPLE2ENH__   ; to save 12 cycles
-        lda     CH
-        bit     RD80VID         ; In 80 column mode?
-        bpl     put             ; No, just go ahead
-        lsr                     ; Div by 2
-        bcs     put             ; Odd cols go in main memory
-        bit     HISCR           ; Assume SET80COL
-put:    tay
+        pha                     ; 16    9      Local, stripped-down version of putchardirect
+        .ifdef  __APPLE2ENH__   ;              to save 12 jsr+rts cycles
+        lda     CH              ;       12
+        bit     RD80VID         ;       16     In 80 column mode?
+        bpl     put             ;       18/19  No, just go ahead
+        lsr                     ;       20     Div by 2
+        bcs     put             ;       22/23  Odd cols go in main memory
+        bit     HISCR           ;       26     Assume SET80COL
+put:    tay                     ;       28
         .else
-        ldy     CH
+        ldy     CH              ; 19
         .endif
-        pla
-        sta     (BASL),Y
+
+        pla                     ; 22    31
+        sta     (BASL),Y        ; 28    36
+
         .ifdef  __APPLE2ENH__
-        bit     LOWSCR          ; Doesn't hurt in 40 column mode
+        bit     LOWSCR          ;       40     Doesn't hurt in 40 column mode
         .endif
-        inc     CH              ; Can't wrap line!
 
-        dex
-        bne     st
+        inc     CH              ; 33    45     Can't wrap line!
 
-        pla                     ; Restore Y
-        tay
+        dex                     ; 35    47
+        bne     st              ; 37/38 49/50
+
+        pla                     ; 40    52     Restore Y
+        tay                     ; 42    54
 
 st2:    SER_AVAIL_A_6           ; Fetch next cycle
         beq     st2
@@ -1484,6 +1491,11 @@ dt2:    SER_FETCH_DEST_A_4
         sta     desttitle+2
 desttitle:
         jmp     $0000
+
+; Vars (in CODE to save a very little bit of space)
+dummy_abs: .res 1
+counter:   .res 1
+prevspd:   .res 1
 
 .align 256
 .assert * = _SAMPLES_BASE+$2200, error
@@ -1499,7 +1511,4 @@ break_out:
         lda     #$2F            ; SURL_CLIENT_READY
         jmp     _serial_putc_direct
 
-        .bss
-dummy_abs: .res 1
-counter:   .res 1
-prevspd:   .res 1
+end_audio_streamer = *
