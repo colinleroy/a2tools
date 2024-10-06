6d1d9cf6181493987685976f55a57c3d6080b209
Colin Leroy-Mira <colin@colino.net>
2024-10-06 11:12:49 +0200

    update ut

--- src/ut/test_strsplit.c
+++ src/ut/test_strsplit.c
@@ -12,23 +12,44 @@ int i;
 /* Full of leaks, but we want a new pointer on each test */
 int main(void) {
   n_lines = strnsplit_in_place(strdup(""), '\n', lines, 3);
+  for (i = 0; i < n_lines; i++) {
+    printf("lines[%d]='%s'\n", i, lines[i]);
+  }
   ASSERT_EQU(n_lines, 0, "wrong number of lines");
 
+  n_lines = strnsplit_in_place(strdup("\n"), '\n', lines, 3);
+  for (i = 0; i < n_lines; i++) {
+    printf("lines[%d]='%s'\n", i, lines[i]);
+  }
+  ASSERT_EQU(n_lines, 1, "wrong number of lines");
+
   n_lines = strnsplit_in_place(strdup("abc"), '\n', lines, 3);
+  for (i = 0; i < n_lines; i++) {
+    printf("lines[%d]='%s'\n", i, lines[i]);
+  }
   ASSERT_EQU(n_lines, 1, "wrong number of lines");
   ASSERT_STR_EQU(lines[0], "abc", "wrong value");
 
   n_lines = strnsplit_in_place(strdup("abc\n"), '\n', lines, 3);
+  for (i = 0; i < n_lines; i++) {
+    printf("lines[%d]='%s'\n", i, lines[i]);
+  }
   ASSERT_EQU(n_lines, 1, "wrong number of lines");
   ASSERT_STR_EQU(lines[0], "abc", "wrong value");
 
   n_lines = strnsplit_in_place(strdup("abc\ndef\nghi\n"), '\n', lines, 3);
+  for (i = 0; i < n_lines; i++) {
+    printf("lines[%d]='%s'\n", i, lines[i]);
+  }
   ASSERT_EQU(n_lines, 3, "wrong number of lines");
   ASSERT_STR_EQU(lines[0], "abc", "wrong value");
   ASSERT_STR_EQU(lines[1], "def", "wrong value");
   ASSERT_STR_EQU(lines[2], "ghi", "wrong value");
 
   n_lines = strnsplit_in_place(strdup("abc\ndef\nghi\n\njkl"), '\n', lines, 5);
+  for (i = 0; i < n_lines; i++) {
+    printf("lines[%d]='%s'\n", i, lines[i]);
+  }
   ASSERT_EQU(n_lines, 5, "wrong number of lines");
   ASSERT_STR_EQU(lines[0], "abc", "wrong value");
   ASSERT_STR_EQU(lines[1], "def", "wrong value");
@@ -37,6 +58,9 @@ int main(void) {
   ASSERT_STR_EQU(lines[4], "jkl", "wrong value");
 
   n_lines = strnsplit_in_place(strdup("abc\ndef\nghi\n\n\n"), '\n', lines, 5);
+  for (i = 0; i < n_lines; i++) {
+    printf("lines[%d]='%s'\n", i, lines[i]);
+  }
   ASSERT_EQU(n_lines, 5, "wrong number of lines");
   ASSERT_STR_EQU(lines[0], "abc", "wrong value");
   ASSERT_STR_EQU(lines[1], "def", "wrong value");
@@ -45,6 +69,9 @@ int main(void) {
   ASSERT_STR_EQU(lines[4], "", "wrong value");
 
   n_lines = strnsplit_in_place(strdup("abc\ndef\nghi\n\n\njkl"), '\n', lines, 6);
+  for (i = 0; i < n_lines; i++) {
+    printf("lines[%d]='%s'\n", i, lines[i]);
+  }
   ASSERT_EQU(n_lines, 6, "wrong number of lines");
   ASSERT_STR_EQU(lines[0], "abc", "wrong value");
   ASSERT_STR_EQU(lines[1], "def", "wrong value");
@@ -54,6 +81,9 @@ int main(void) {
   ASSERT_STR_EQU(lines[5], "jkl", "wrong value");
 
   n_lines = strnsplit_in_place(strdup("abc\ndef\nghi\n\njkl\n"), '\n', lines, 2);
+  for (i = 0; i < n_lines; i++) {
+    printf("lines[%d]='%s'\n", i, lines[i]);
+  }
   ASSERT_EQU(n_lines, 2, "wrong number of lines");
   ASSERT_STR_EQU(lines[0], "abc", "wrong value");
   ASSERT_STR_EQU(lines[1], "def", "wrong value");
