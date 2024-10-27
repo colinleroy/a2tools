3a561a8653daaffb3faf481a426afa5fd99c23d7
Colin Leroy-Mira <colin@colino.net>
2024-10-27 11:31:29 +0100

    Rewrite Fujinet API to SURL protocol

--- src/weather/ipapi.c
+++ src/weather/ipapi.c
@@ -11,14 +11,15 @@
 
 #include <conio.h>
 
-#include "fujinet-network.h"
 #include "weatherdefs.h"
 #include "weatherdisp.h"
 #include "ipapi.h"
 
+#include "surl.h"
+
 extern int	err;
 
-char ip_url[] = "N:http://ip-api.com/json/?fields=status,city,countryCode,lon,lat";
+char ip_url[] = "http://ip-api.com/json/?fields=status,city,countryCode,lon,lat";
 
 
 //
@@ -30,23 +31,21 @@ void get_location(LOCATION *loc) {
 	char	buf[LINE_LEN];
 	char	message[LINE_LEN];
 
-	network_open(ip_url, OPEN_MODE_READ, OPEN_TRANS_NONE);
-    err = network_json_parse(ip_url);
-    handle_err("ip-api parse");
+	surl_start_request(NULL, 0, ip_url, SURL_METHOD_GET);
+	
+	err = !surl_response_ok();
+	handle_err("ip-api parse");
 
-	network_json_query(ip_url, "/status", buf);
+	surl_get_json(buf, ".status", "ISO646-FR1", 0, LINE_LEN);
 	if (strcmp(buf, "success") != 0) {
-		network_json_query(ip_url, "/message", buf);
-		network_close(ip_url);
+		surl_get_json(buf, ".message", "ISO646-FR1", 0, LINE_LEN);
 		sprintf(message, "ip-api(%s)", buf);
 		err = 0xff;					// set unknown error 
-    	handle_err(message);
+		handle_err(message);
 	}
 
-	network_json_query(ip_url, "/city", loc->city);
-	network_json_query(ip_url, "/countryCode", loc->countryCode);
-	network_json_query(ip_url, "/lon", loc->lon);
-	network_json_query(ip_url, "/lat", loc->lat);
-
-	network_close(ip_url);
+	surl_get_json(loc->city, ".city", "ISO646-FR1", 0, HALF_LEN);
+	surl_get_json(loc->countryCode, ".countryCode", "ISO646-FR1", 0, QUARTER_LEN);
+	surl_get_json(loc->lon, ".lon", "ISO646-FR1", 0, HALF_LEN);
+	surl_get_json(loc->lat, ".lat", "ISO646-FR1", 0, HALF_LEN);
 }
