6e222ce753063fcd2e77bf4e1554c55c814c9ad6
Colin Leroy-Mira <colin@colino.net>
2025-02-17 11:36:09 +0100

    Cleanup and micro-opt

--- src/test/draw.s
+++ src/test/draw.s
@@ -1,6 +1,6 @@
-        .export _draw_palette, _clear_palette
+        .export _draw_palette, _clear_and_draw_palette
 
-        .importzp _zp6p, _zp8p, _zp10, _zp11, _zp12, ptr1
+        .importzp _zp6p, _zp8, _zp9, _zp10
         .import   mouse_x, mouse_y
         .import   _hgr_baseaddr
         .import   _palette
@@ -8,24 +8,22 @@
         .include "apple2.inc"
         .include "palette.inc"
 
-sprite     = _zp6p
-line       = _zp8p
+line       = _zp6p
 
-n_lines    = _zp10
-sprite_num = _zp11
-cur_y      = _zp12
+n_lines    = _zp8
+sprite_num = _zp9
+cur_y      = _zp10
 
-_clear_palette:
-        lda     #14
+_clear_and_draw_palette:
+        lda     #(PALETTE_HEIGHT-1)
         sta     n_lines
 
-        lda     mouse_prev_y
-        sta     cur_y
+        ldy     mouse_prev_y
+        sty     cur_y
 
         ldx     #(PALETTE_BYTES-1)
 
         clc
-        ldy     cur_y
 clear_next_line:
         lda     mouse_prev_x
         adc     _hgr_baseaddr+256,y
@@ -34,7 +32,7 @@ clear_next_line:
         adc     #0
         sta     line+1
 
-        ldy     #5
+        ldy     #((PALETTE_BYTES/PALETTE_HEIGHT)-1)
 
 :       lda     palette_backup,x
         sta     (line),y
@@ -49,8 +47,7 @@ clear_next_line:
         dec     n_lines
         bpl     clear_next_line
 
-        rts
-
+        ; Fallback to draw_palette
 
 _draw_palette:
         lda     mouse_x
@@ -62,7 +59,7 @@ _draw_palette:
         lda     _palette+1,x
         sta     p_pointer+2
 
-        lda     #14
+        lda     #(PALETTE_HEIGHT-1)
         sta     n_lines
 
         lda     mouse_y
@@ -86,13 +83,13 @@ next_line:
         adc     #0
         sta     line+1
 
-        ldy     #5
+        ldy     #((PALETTE_BYTES/PALETTE_HEIGHT)-1)
 
 :       lda     (line),y
         sta     palette_backup,x
         ; draw palette
 p_pointer:
-        lda     $FFFF,x
+        lda     $FFFF,x       ; Patched
         sta     (line),y
         dex
         dey
