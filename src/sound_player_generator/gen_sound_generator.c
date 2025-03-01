1a280ee23e8f3249b41251a68eb5a1298bd59ed0
Colin Leroy-Mira <colin@colino.net>
2025-03-01 15:08:50 +0100

    Align data correctly

--- src/sound_player_generator/gen_sound_generator.c
+++ src/sound_player_generator/gen_sound_generator.c
@@ -178,6 +178,8 @@ int main(int argc, char *argv[]) {
   }
 
   /* Player */
+  printf("         .data\n\n"
+         ".align $100\n");
 #ifdef CPU_65c02
   printf("_play_sample:\n"
          "         sta ptr1\n"
