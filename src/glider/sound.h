1239b72979f2ebbda5b8163d2115d51e2c1e0b60
Colin Leroy-Mira <colin@colino.net>
2025-03-01 13:10:49 +0100

    Make it work (less good) on 6502

--- src/glider/sound.h
+++ src/glider/sound.h
@@ -1,8 +1,17 @@
 #define CYCLES_PER_SEC 1023000
 /* Determine the number of available cycles according to the desired carrier */
-#define AVAIL_CYCLES   ((CYCLES_PER_SEC/CARRIER_HZ)-9) /* 9 is the overhead of the jmp($nnnn,x)+jmp back */
+
+#ifdef CPU_65c02
+/* 9 is the overhead of the jmp($nnnn,x)+jmp back */
+#define AVAIL_CYCLES   ((CYCLES_PER_SEC/CARRIER_HZ)-9)
+#else
+/* 9 is the overhead of the jmp($nnnn,x)+jmp back,
+ * 13 is the overhead of the lda/sta replacing the jmp ($nnnn,x) */
+#define AVAIL_CYCLES   ((CYCLES_PER_SEC/CARRIER_HZ)-9-13)
+#endif
 
 #define NUM_LEVELS     (AVAIL_CYCLES - 28) /* Overhead of the reading */
+
 #define STEP           1
 
 #define PAGE_CROSSER   0x32
