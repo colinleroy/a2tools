da00c47d4f31d7770b05e933add3eb4db469b5e7
Colin Leroy-Mira <colin@colino.net>
2025-03-07 22:34:27 +0100

    Add Bulgarian translation, thanks to @circfruit@fosstodon.org

---
+++ src/glider/code/strings-bg.s
@@ -0,0 +1,15 @@
+        .export   _time_bonus_str
+        .export   _your_score_str
+        .export   _press_key_str
+        .export   _no_level_str
+        .export   _game_won_str
+        .export   _game_lost_str
+
+        .data
+
+_time_bonus_str:  .asciiz "           BONUS WREME:   "
+_your_score_str:  .asciiz "                 TOXKI:   "
+_press_key_str:   .asciiz "NATISNI KLAVIV ZA NIVO:   "
+_no_level_str:    .asciiz "              NQMA NIWO   "
+_game_won_str:    .asciiz "      PREWYRTQ IGRATA!"
+_game_lost_str:   .asciiz "       IGRATA SWYRVI ! :-("
