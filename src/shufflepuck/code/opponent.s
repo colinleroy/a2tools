ab127e0b03352058cb580ccdd5cc4d0031bda1c4
Colin Leroy-Mira <colin@colino.net>
2025-04-03 08:07:53 +0200

    Let opponent serve a bit funnier

--- src/shufflepuck/code/opponent.s
+++ src/shufflepuck/code/opponent.s
@@ -17,41 +17,80 @@
 
         .import     their_pusher_x, their_pusher_y
         .import     their_pusher_dx, their_pusher_dy
-        .import     puck_x, puck_y, serving
+        .import     puck_x, puck_y, puck_dy, serving
 
         .importzp   tmp1
 
         .include    "sprite.inc"
         .include    "their_pusher_coords.inc"
         .include    "puck_coords.inc"
+        .include    "my_pusher0.gen.inc"
+        .include    "puck0.gen.inc"
         .include    "constants.inc"
 
 THEIR_MAX_DX       = 4
 THEIR_MAX_DY       = 8
 
+.proc invert_pusher_dx
+        lda     their_pusher_dx
+        clc
+        eor     #$FF
+        adc     #$01
+        sta     their_pusher_dx
+        rts
+.endproc
+
 .proc _opponent_think
         lda     serving
-        beq     catch
+        beq     serve_or_catch
 
-        ; We're serving
+prepare_service:
+        cmp     #$01
+        bne     :+
+
+        ; Who serves?
+        lda     puck_y
+        cmp     #THEIR_PUCK_INI_Y
+        bne     serve_or_catch    ; It's the player
+
+        ; Init serve parameters
+        lda     #(ABS_MAX_DX)
+        sta     their_pusher_dx
+
+:       ; We're serving
         inc     serving
-        beq     hit               ; After a while we got to serve
+        beq     serve_or_catch      ; After a while we got to serve
 
         lda     their_pusher_y
         cmp     #(THEIR_PUSHER_MIN_Y+1)
         bcs     :+
+
         lda     #$01
         sta     their_pusher_dy
+        jsr     invert_pusher_dx
+
         rts
 :       cmp     #(THEIR_PUCK_INI_Y-2)
         bcs     :+
         rts
-:       lda     #$FF
+:
+
+        lda     #$FF
         sta     their_pusher_dy
+        jsr     invert_pusher_dx
         rts
+
+serve_or_catch:
+        lda     puck_dy           ; Is the puck moving?
+        bne     catch
+
+        lda     #$00              ; Cancel Y speed
+        sta     their_pusher_dy
 
 catch:
         lda     their_pusher_x
+        clc                       ; Center puck on pusher
+        adc     #((my_pusher0_WIDTH-puck0_WIDTH)/2)
         cmp     puck_x
         bcs     move_left
 
@@ -81,14 +120,24 @@ move_left:
 store_dx:
         sta     their_pusher_dx
 
-        ; Update DY now
-        lda     puck_y
+        ; Update DY now if we can
+        lda     puck_dy           ; Is the puck moving?
+        bne     :+
+
+        lda     puck_y            ; Is it possible to hit the puck if we move forward now?
+        sec
+        sbc     their_pusher_y
+        cmp     their_pusher_dx
+        bcs     :+
+        rts                       ; No, so return
+
+:       lda     puck_y
         cmp     #MY_PUSHER_MAX_Y   ; Move forward as long as puck isn't in the end of the player zone (defensive)
         ;cmp     #MY_PUSHER_MIN_Y   ; Move forward as long as puck isn't in the player zone (a bit dangerous)
         ;cmp     #MID_BOARD         ; Move forward as long as puck isn't in the player half of the board (dangerous)
         ;cmp     #THEIR_PUSHER_MAX_Y; Move forward as long as puck isn't in our zone (very dangerous)
         bcs     move_forwards_slow
-        cmp     #THEIR_PUSHER_MAX_Y
+        cmp     #(THEIR_PUSHER_MAX_Y)
         bcs     move_backwards
         ; Otherwise prepare to hit
 hit:
