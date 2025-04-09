fb4ec5488ad7471f32c6c0fdc139abfd7a8bab6a
Colin Leroy-Mira <colin@colino.net>
2025-04-09 22:16:47 +0200

    Start working on Nerual
    Sprites done, no sound, magic band done, service parameters stored

---
+++ src/shufflepuck/opponents/opponentf.s
@@ -0,0 +1,311 @@
+; Copyright (C) 2025 Colin Leroy-Mira <colin@colino.net>
+;
+; This program is free software; you can redistribute it and/or modify
+; it under the terms of the GNU General Public License as published by
+; the Free Software Foundation; either version 3 of the License, or
+; (at your option) any later version.
+;
+; This program is distributed in the hope that it will be useful,
+; but WITHOUT ANY WARRANTY; without even the implied warranty of
+; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+; GNU General Public License for more details.
+;
+; You should have received a copy of the GNU General Public License
+; along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+; ----------
+; NERUAL
+; How to beat: Hit real hard and hope for the best
+
+        .import     their_pusher_x, their_pusher_y
+        .import     their_pusher_dx, their_pusher_dy
+        .import     their_currently_hitting, my_currently_hitting
+        .import     puck_x, puck_right_x, puck_y, puck_dx, puck_dy, serving, their_score
+        .import     _guess_puck_x_at_y
+        .import     _rand
+
+        .import     _big_draw_sprite_f                              ; CHANGE A
+        .import     _big_draw_name_f                                ; CHANGE A
+        .import     _big_draw_normal_f                              ; CHANGE A
+        .import     _big_draw_lose_f                                ; CHANGE A
+        .import     _big_draw_win_f                                 ; CHANGE A
+        .import     _play_win_f                                     ; CHANGE A
+        .import     _play_lose_f                                    ; CHANGE A
+        .import     _play_serve_f                                   ; CHANGE A
+
+        .import     __OPPONENT_START__
+        .importzp   tmp1
+
+        .include    "apple2.inc"
+        .include    "my_pusher0.gen.inc"
+        .include    "puck0.gen.inc"
+        .include    "../code/sprite.inc"
+        .include    "../code/their_pusher_coords.inc"
+        .include    "../code/puck_coords.inc"
+        .include    "../code/constants.inc"
+        .include    "../code/opponent_file.inc"
+
+.segment "f"                                                            ; CHANGE A
+
+.assert * = __OPPONENT_START__+OPPONENT::SPRITE, error ; Make sure the callback is where we think
+sprite:
+        ldx     #(98/7)
+        ldy     #76
+        jmp     _big_draw_sprite_f                                      ; CHANGE A
+
+.assert * = __OPPONENT_START__+OPPONENT::NAME, error ; Make sure the callback is where we think
+name:
+        ldx     #(7/7)
+        ldy     #39
+        jmp     _big_draw_name_f                                        ; CHANGE A
+
+.assert * = __OPPONENT_START__+OPPONENT::LOSE_POINT, error ; Make sure the callback is where we think
+lose_animation:
+        ldx     #((21+98)/7)    ; left X of sprite change + left X of big sprite
+        ldy     #(76)           ; bottom Y of sprite change
+        jmp     _big_draw_lose_f                                        ; CHANGE A
+
+.assert * = __OPPONENT_START__+OPPONENT::LOSE_POINT_SND, error ; Make sure the callback is where we think
+lose_sound:
+        ldy     #0
+        jmp     _play_lose_f                                            ; CHANGE A
+
+.assert * = __OPPONENT_START__+OPPONENT::WIN_POINT, error ; Make sure the callback is where we think
+win_animation:
+        ldx     #((21+98)/7)    ; left X of sprite change + left X of big sprite
+        ldy     #(76)           ; bottom Y of sprite change
+        jmp     _big_draw_win_f                                        ; CHANGE A
+
+.assert * = __OPPONENT_START__+OPPONENT::WIN_POINT_SND, error ; Make sure the callback is where we think
+win_sound:
+        ldy     #0
+        jmp     _play_win_f                                            ; CHANGE A
+
+.assert * = __OPPONENT_START__+OPPONENT::THINK_CB, error ; Make sure the callback is where we think
+.proc _opponent_think
+        lda     serving
+        beq     serve_or_catch
+
+init_service:
+        cmp     #$01
+        bne     prepare_service
+
+        lda     #$00
+        sta     found_x
+
+        ; Who serves?
+        lda     puck_y
+        cmp     #THEIR_PUCK_INI_Y
+        beq     :+
+        ldx     #1                 ; It's the player, we need to remember the service
+        stx     remember_service
+        dex
+        stx     player_service_dx
+        stx     player_service_dy
+        beq     serve_or_catch
+:
+
+        ldy     #0
+        sty     remember_service
+        jsr     _play_serve_f                                          ; CHANGE A
+; -------
+; End of opponent letter references
+; -------
+
+        ; Init serve parameters
+        lda     #0
+        sta     their_pusher_dx
+
+        ; Shorten wait
+        jsr     _rand
+        beq     prepare_service
+        sta     serving
+
+prepare_service:
+        ; We're serving
+        inc     serving
+        beq     serve_or_catch      ; After a while we got to serve
+
+        lda     their_pusher_y
+        cmp     #(THEIR_PUSHER_MIN_Y+1)
+        bcs     :+
+
+        lda     #$01
+        sta     their_pusher_dy
+
+        rts
+:       cmp     #(THEIR_PUCK_INI_Y-2)
+        bcs     :+
+        rts
+:
+
+        lda     #$FF
+        sta     their_pusher_dy
+        rts
+
+serve_or_catch:
+        lda     puck_dy           ; Is the puck moving?
+        bne     catch
+
+        lda     #$00              ; Cancel Y speed
+        sta     their_pusher_dy
+
+catch:
+        ; Register the player's service parameters
+        ; if we don't have them already
+        lda     remember_service
+        beq     :+
+        ldx     puck_dx
+        beq     :+
+        ldy     puck_dy
+        beq     :+
+        stx     player_service_dx
+        sty     player_service_dy
+        lda     #$00
+        sta     remember_service
+
+:       ; Is it time to figure out destination X?
+        lda     puck_dy
+        bpl     move
+
+        lda     puck_y
+        ; Magic band where guessing X is allowed
+        cmp     #129
+        bcs     move
+        cmp     #122
+        bcc     move
+
+        ; Figure X if not already done
+        lda     found_x
+        bne     move
+        lda     #25
+        jsr     _guess_puck_x_at_y
+
+        ; And store it (but not 0, so we know we found it)
+        tax
+        bne     :+
+        inx
+:       stx     found_x
+
+move:   
+        lda     found_x
+        beq     follow_puck
+
+        ; Go directly where the puck will be when hittable
+        lda     found_x
+        cmp     #puck0_WIDTH
+        bcs     :+
+        lda     #0
+        beq     store_x
+:       cmp     #(PUCK_MAX_X-puck0_WIDTH)
+        bcc     store_x
+        lda     #THEIR_PUSHER_MAX_X-1
+        bne     store_x
+
+        sec
+        sbc     #((my_pusher0_WIDTH-puck0_WIDTH)/2)
+store_x:
+        sta     their_pusher_x
+        
+        lda     #$00
+        jmp     store_dx
+
+follow_puck:
+        lda     their_pusher_x
+        clc                       ; Center puck on pusher
+        adc     #((my_pusher0_WIDTH-puck0_WIDTH)/2)
+        sta     mid_pusher_x
+        sec
+        cmp     puck_x
+        bcs     move_left
+
+move_right:
+        lda     puck_x
+        sec
+        sbc     mid_pusher_x
+
+        cmp     their_max_dx
+        bcc     store_dx
+        lda     their_max_dx
+        clc
+        jmp     store_dx
+
+move_left:
+        lda     mid_pusher_x
+        sec
+        sbc     puck_x
+
+        cmp     their_max_dx
+        bcc     :+
+        lda     their_max_dx
+
+:       clc
+        eor     #$FF
+        adc     #$01
+store_dx:
+        sta     their_pusher_dx
+
+        ; Did we just hit?
+        lda     their_currently_hitting
+        bne     move_backwards
+
+        ; Can we hit now?
+        lda     their_pusher_x
+        cmp     puck_right_x
+        bcs     move_backwards
+
+        clc
+        adc     #my_pusher0_WIDTH ; Same for their pusher, they are the same size
+        bcs     :+                ; If adding our width overflowed, we're good
+        cmp     puck_x            ; the pusher is on the right and the puck too
+        bcc     move_backwards
+
+:       lda     puck_y
+        cmp     #MY_PUSHER_MAX_Y   ; Move forward as long as puck isn't in the end of the player zone (defensive)
+        bcs     move_forwards_slow
+        cmp     #(THEIR_PUSHER_MAX_Y)
+        bcs     move_backwards
+hit:
+        ; Forget found X
+        lda     #0
+        sta     found_x
+
+        ; Get a 4-11 DY
+        jsr     _rand
+        lsr
+        lsr
+        lsr
+        lsr
+        lsr
+        clc
+        adc     #4
+        sta     their_pusher_dy
+        rts
+
+move_forwards_slow:
+        lda     #3
+        sta     their_pusher_dy
+        rts
+
+move_backwards:
+        lda     #<-6
+        sta     their_pusher_dy
+        rts
+.endproc
+
+.proc invert_pusher_dx
+        lda     their_pusher_dx
+        clc
+        eor     #$FF
+        adc     #$01
+        sta     their_pusher_dx
+        rts
+.endproc
+
+their_max_dx:     .byte 5
+their_max_dy:     .byte 4
+mid_pusher_x:     .byte 1
+found_x:          .byte 0
+player_service_dx:.byte 0
+player_service_dy:.byte 0
+remember_service: .byte 0
