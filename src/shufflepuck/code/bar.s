43866017270d52e38a45a62935073c05c998979c
Colin Leroy-Mira <colin@colino.net>
2025-04-12 16:36:40 +0200

    Add Biff

--- src/shufflepuck/code/bar.s
+++ src/shufflepuck/code/bar.s
@@ -32,6 +32,7 @@ opponents_boxes:                      ; X, W, Y, H, opponent number
         .byte 73,  38, 94,  36, 4     ; Eneg
         .byte 7,   28, 21,  39, 5     ; Nerual
         .byte 129, 24, 58,  41, 6     ; Bejin
+        .byte 245, 10, 77,  51, 7     ; Biff
         .byte 83,  87, 12,  34, $FF   ; Tournament
         .byte 231, 21, 13,  21, CH_ESC; Exit
 NUM_BOXES = (* - BOXES_START)/5
