4c7a079e4816683faa48ef1cc516827a8e4930d6
Colin Leroy-Mira <colin@colino.net>
2025-04-21 22:09:33 +0200

    Comment

--- src/shufflepuck/code/bar.s
+++ src/shufflepuck/code/bar.s
@@ -61,6 +61,7 @@ congrats_str: .asciiz "CONGRATS! PLEASE ENTER YOUR NAME:"
 fight_str:    .asciiz "TOURNAMENT"
 view_str:     .asciiz "VIEW ROSTER"
 
+; Copy first string from scores buffer to the champion string
 .proc set_champion
         lda     #<champion_str
         ldx     #>champion_str
@@ -70,38 +71,41 @@ view_str:     .asciiz "VIEW ROSTER"
         jmp     _strcpy
 .endproc
 
+; Print the champion on the board
 .proc print_champion
         jsr     clear_champion
         lda     #<champion_str
         ldx     #>champion_str
         jsr     pushax            ; Push for print_string
 
-        jsr     _strlen           ; Center string
+        jsr     _strlen           ; Center string: strlen of name
         sta     tmp1
-        lda     #CHAMPION_LEN
+        lda     #CHAMPION_LEN     ; max len minus strlen
         sec
         sbc     tmp1
-        lsr
+        lsr                       ; divided by two
         clc
-        adc     #(CHAMPION_LEFT/7)
+        adc     #(CHAMPION_LEFT/7); Add left X
         tax
         ldy     #CHAMPION_BOTTOM
         jmp     _print_string
 .endproc
 
+; Clears the Champion sign
 .proc clear_champion
         lda     #<empty_str
         ldx     #>empty_str
+        jsr     pushax            ; Push twice, once for each line
         jsr     pushax
-        jsr     pushax
-        ldx     #(CHAMPION_LEFT/7)
+        ldx     #(CHAMPION_LEFT/7); Clear line 1
         ldy     #MENU_BOTTOM
         jsr     _print_string
-        ldx     #(CHAMPION_LEFT/7)
+        ldx     #(CHAMPION_LEFT/7); Clear line 2
         ldy     #CHAMPION_BOTTOM
         jmp     _print_string
 .endproc
 
+; Prints the menu (TOURNAMENT / VIEW ROSTER) on the Champion sign
 .proc print_menu
         jsr     clear_champion
         lda     #<fight_str
@@ -118,8 +122,10 @@ view_str:     .asciiz "VIEW ROSTER"
         jmp     _print_string
 .endproc
 
+; Wait for user to click on an opponent (or press Escape)
 .proc _choose_opponent
-        ; Only reprint Champion board when needed
+        ; Only reprint Champion board when needed: when entering first,
+        ; and when the display changes
         jsr     clear_pointer
         lda     in_menu
         beq     show_champion
@@ -128,44 +134,51 @@ view_str:     .asciiz "VIEW ROSTER"
 show_champion:
         jsr     print_champion
 
+; Loop waiting for input
 wait_input:
-        jsr     _mouse_wait_vbl
+        jsr     _mouse_wait_vbl   ; Avoid flickering
         lda     mouse_x
         sta     pointer_x
         lda     mouse_y
         sta     pointer_y
         jsr     clear_pointer
         jsr     draw_pointer
-
+   
+        ; Check for key
         jsr     _check_keyboard
         bcs     kbd
+
+        ; Or for mouse button
         jsr     _mouse_check_button
         bcc     wait_input
 
-        ; Wait for button to be up
+        ; Wait for button to be up (otherwise the Champion sign flickers)
 :       jsr     _mouse_check_button
         bcs     :-
 
+        ; Is the champion menu open?
         lda     in_menu
         beq     search_opponent
 
+        ; Yes: check the user choice (TOURNAMENT, ROSTER, or out of the menu)
         jsr     check_menu_choice
-        bcc     close_menu
-        bmi     return_opponent   ; User chose TOURNAMENT
+        bcc     close_menu        ; User clicked outside of menu
+        bmi     return_opponent   ; User chose TOURNAMENT (and A=$FF)
 
-        jsr     show_roster       ; User chose ROSTER (and A=0)
-
+        jsr     show_roster       ; User chose ROSTER (and A=0 so "add" flag
+                                  ; is off). ). Show the roster,
 close_menu:
-        lda     #0
+        lda     #0                ; And close menu
         sta     in_menu
-        beq     _choose_opponent  ; Branch always
+        beq     _choose_opponent  ; Branch always to start of function, in
+                                  ; order to reprint Champion sign
 
 search_opponent:
-        jsr     find_opponent
-        bcc     wait_input
+        jsr     find_opponent     ; Check if mouse in an opponent's hitbox
+        bcc     wait_input        ; No, go wait for input again
 
         ; Did the user click the Champion sign?
-        bmi     open_menu
+        bmi     open_menu         ; Yes, open the menu
 
 return_opponent:                  ; Return the opponent number
         pha                       ; or $FF to start tournament
@@ -174,17 +187,14 @@ return_opponent:                  ; Return the opponent number
         rts
 
 kbd:
-        cmp     #CH_ESC
-        bne     wait_input
-        jmp     _exit
+        cmp     #CH_ESC           ; Same value as EXIT sign?
+        bne     wait_input        ; No, go wait for input again
+        jmp     _exit             ; Yes, exit
 
 open_menu:
-        lda     #1
-        sta     in_menu
-        jmp     _choose_opponent
-
-handle_menu_choice:
-        brk
+        lda     #1                ; Flag menu open and jump back
+        sta     in_menu           ; to start of function in order to reprint
+        jmp     _choose_opponent  ; the Champion sign
 .endproc
 
 .proc find_opponent
@@ -193,10 +203,10 @@ check_next_opponent:
         lda     opponents_boxes,y ; check left
         cmp     mouse_x
         iny
-        bcs     skip_y
+        bcs     skip_y_check
         adc     opponents_boxes,y ; check left+width
         cmp     mouse_x
-        bcc     skip_y
+        bcc     skip_y_check
 
         iny
         lda     opponents_boxes,y ; check top
@@ -212,14 +222,17 @@ check_next_opponent:
         sec
         rts
 
-skip_y: iny
-        iny
-skip_num_opponent:
+skip_y_check:                     ; X didn't match so skip Y,H
         iny
         iny
+
+skip_num_opponent:                ; Y didn't match so skip opponent
+        iny
+        iny                       ; and advance to next hitbox
+
         cpy     #((NUM_BOXES*5))
         bne     check_next_opponent
-        clc
+        clc                       ; All hitboxes checked, none matched
         rts
 .endproc
 
@@ -238,7 +251,7 @@ skip_num_opponent:
 .endproc
 
 .proc _add_hall_of_fame
-        lda     #1
+        lda     #1                ; Set "add" flag and show roster
         jmp     show_roster
 .endproc
 
@@ -249,16 +262,17 @@ skip_num_opponent:
 .proc check_menu_choice
         lda     mouse_x
         cmp     #CHAMPION_LEFT
-        bcc     out
+        bcc     out               ; Check X is on the right of the Champion sign's left border
         cmp     #(CHAMPION_LEFT+(CHAMPION_LEN*7))
-        bcs     out
+        bcs     out               ; and on the left of its right border
+
         lda     mouse_y
         cmp     #MENU_TOP
-        bcc     out
+        bcc     out               ; Check Y is under the top
         cmp     #CHAMPION_BOTTOM
-        bcc     do_tournament
+        bcc     do_tournament     ; and over the end of the TOURNAMENT line
         cmp     #MENU_BOTTOM
-        bcc     view_roster
+        bcc     view_roster       ; or over the end of the VIEW ROSTER LINE
 out:    clc
         rts
 
@@ -279,34 +293,36 @@ view_roster:
         pla
 
         beq     :+
-        jsr     shift_score_out
+        jsr     shift_score_out   ; We should add a name, so drop the last one
 
-        ; Set our score
-        lda     my_score
+        lda     my_score          ; Set our score (could be hardcoded, it has to be 15)
         sta     _scores_buffer
-        lda     their_score
+        lda     their_score       ; Set Biff's score
         sta     _scores_buffer+1
 
-        jsr     display_scores
-        jsr     add_name
+        jsr     display_scores    ; Display the score
+        jsr     add_name          ; Let user add their name
 
         lda     #<_scores_buffer
         ldx     #>_scores_buffer
-        jsr     _save_scores
-        jsr     set_champion
+        jsr     _save_scores      ; Save the file
+        jsr     set_champion      ; Update the Champion sign
+
         ; Re-backup our code so that the scores array
-        ; is updated
+        ; is updated in the backup
         jsr     _backup_bar_code
+
+        ; And restore the bar image
         jmp     _restore_bar
 
-:       jsr     display_scores
-        jsr     _mouse_check_button
+:       jsr     display_scores      ; Don't add name, just list scores.
+        jsr     _mouse_check_button ; and wait for button click
         bcs     out
-        lda     KBD
+        lda     KBD                 ; or keypress
         bpl     :-
         bit     KBDSTRB
 
-out:    jmp     _restore_bar
+out:    jmp     _restore_bar        ; and restore bar image
 .endproc
 
 SCORE_LINE_X_LEFT  = 14/7
@@ -315,24 +331,25 @@ SCORE_LINE_X_RIGHT = 154/7
 SCORE_LINE_X_RIGHT_SCORE = MAX_WINNER_LEN+SCORE_LINE_X_RIGHT
 SCORE_LINE_Y_TOP   = 48-(font_HEIGHT+2)
 
+; Add player name to the scores array
 .proc add_name
-        lda     #<congrats_str
+        lda     #<congrats_str      ; Show the congrats line
         ldx     #>congrats_str
         jsr     pushax
         ldx     #SCORE_LINE_X_LEFT
         ldy     #SCORE_LINE_Y_TOP
         jsr     _print_string
 
-        ldx     #SCORE_LINE_X_LEFT
+        ldx     #SCORE_LINE_X_LEFT  ; Go to the first score line
         lda     #SCORE_LINE_Y_TOP
         clc
         adc     #font_HEIGHT+2
         tay
         lda     #MAX_WINNER_LEN-1
-        jsr     _read_string
+        jsr     _read_string        ; Read the name from keyboard
 
-        lda     #<(_scores_buffer+2)
-        ldx     #>(_scores_buffer+2)
+        lda     #<(_scores_buffer+2); Copy it to the scores array first line,
+        ldx     #>(_scores_buffer+2); Right after the two score bytes
         jsr     pushax
         lda     #<_str_input
         ldx     #>_str_input
@@ -360,34 +377,33 @@ SCORE_LINE_Y_TOP   = 48-(font_HEIGHT+2)
 .endproc
 
 .proc display_scores
-        ldy     #SCORE_LINE_Y_TOP
+        ldy     #SCORE_LINE_Y_TOP   ; Start at top
         sty     cur_y
 
-        lda     #<_scores_buffer
+        lda     #<_scores_buffer    ; Set the buffer walker
         ldx     #>_scores_buffer
         sta     ptr2
         stx     ptr2+1
 
-        lda     #$00
+        lda     #$00                ; Store the current index
         sta     cur_print
 next_score:
-        ; get left coordinate
-        ldx     #SCORE_LINE_X_LEFT
-        stx     cur_x               ; Store X
-        ldx     #SCORE_LINE_X_SCORE
-        stx     cur_x_score
+        ldx     #SCORE_LINE_X_LEFT  ; Get left coordinate for the name
+        stx     cur_x               ; Store it
+        ldx     #SCORE_LINE_X_SCORE ; Get left coordinate for the score
+        stx     cur_x_score         ; Store it
 
-        cmp     #(MAX_WINNERS/2)
-        bcc     :+                  ; Right column
-        ldx     #SCORE_LINE_X_RIGHT
-        stx     cur_x               ; Store X
+        cmp     #(MAX_WINNERS/2)    ; Check which column we should use (A = current index)
+        bcc     :+
+        ldx     #SCORE_LINE_X_RIGHT ; Right column, update left coordinates
+        stx     cur_x
         ldx     #SCORE_LINE_X_RIGHT_SCORE
         stx     cur_x_score
 
-        cmp     #(MAX_WINNERS/2)    ; First on right col, reset Y
+        cmp     #(MAX_WINNERS/2)    ; First line on right column?
         bne     :+
 
-        ldy     #SCORE_LINE_Y_TOP
+        ldy     #SCORE_LINE_Y_TOP   ; Yes, reset Y
         sty     cur_y
 
 :       lda     cur_y               ; Update Y
@@ -400,48 +416,48 @@ print_one_score:
         ldy     #$00
         sty     bcd_input+1
         lda     (ptr2),y
-        beq     out               ; We're done
+        beq     out               ; If winner score is 0, we're done
         ldx     cur_x_score
         ldy     cur_y
-        jsr     _print_number
-        ; Separator
-        lda     #'/'
+        jsr     _print_number     ; Print it
+
+        lda     #'/'              ; Separator
         ldx     cur_x_score
         inx
         inx
         jsr     _print_char
-        ; Their score
-        ldy     #$01
+
+        ldy     #$01              ; Biff's score
         lda     (ptr2),y
         ldy     cur_y
         jsr     _print_number
 
-        ; Name
-        lda     ptr2
+        lda     ptr2              ; Player name
         ldx     ptr2+1
         clc
-        adc     #2
+        adc     #2                ; Skip the two score bytes
         bcc     :+
         inx
 :       jsr     pushax
         ldx     cur_x
         ldy     cur_y
-        jsr     _print_string
+        jsr     _print_string     ; Print the name
 
-        lda     ptr2
+        lda     ptr2              ; Push walker to next line
         clc
         adc     #<.sizeof(SCORE_LINE)
         bcc     :+
         inc     ptr2+1
 :       sta     ptr2
 
-        inc     cur_print
+        inc     cur_print         ; Increment index
         lda     cur_print
         cmp     #MAX_WINNERS
         bne     next_score
 out:    rts
 .endproc
 
+; Load the scores array from disk if not already done
 .proc _bar_load_scores
         lda     _scores_loaded
         bne     :+
@@ -454,6 +470,7 @@ out:    rts
 :       rts
 .endproc
 
+; Load scores if needed, and print Champion's name
 .proc _bar_update_champion
         jsr     _bar_load_scores
         lda     _scores_loaded
