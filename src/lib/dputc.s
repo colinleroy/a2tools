43cbd6aab8ec395ba548301d9ed36a982d182ceb
Colin Leroy-Mira <colin@colino.net>
2025-05-24 14:30:10 +0200

    Remove APPLE2ENH

--- src/lib/dputc.s
+++ src/lib/dputc.s
@@ -18,6 +18,7 @@ bell:
 
         .export         _dputcxy, _dputc
         .import         _scrollup_one, gotoxy, FVTABZ, _cputc
+        .import         uppercasemask, machinetype
 
 _dputcxy:
         pha                     ; Save C
@@ -37,25 +38,21 @@ _dputc:
         cmp     CV
         bne     noscroll
 
-.ifdef __APPLE2ENH__
+        bit     machinetype
+        bpl     get40
         bit     RD80VID
         bpl     get40
         lda     OURCH              ; Don't scroll if first line but not first char
-        bra     check_scroll
+        jmp     check_scroll
 get40:
-.endif
         lda     CH
 check_scroll:
         bne     noscroll
 
         jsr     _scrollup_one
         lda     WNDBTM
-        .ifdef __APPLE2ENH__
-        dec     a
-        .else
         sec
         sbc     #1
-        .endif
         sta     CV
         jsr     FVTABZ
 
@@ -66,16 +63,23 @@ noscroll:
 
         .export         _dputcxy, _dputc
         .import         gotoxy, FVTABZ
-        .ifdef  __APPLE2ENH__
-        .import         putchardirect
-        .else
-        .import         putchar
-        .endif
+        .import         putchar, uppercasemask
         .import         _scrollup_one
+        .import         machinetype
 
         .code
 
 ; Plot a character - also used as internal function
+
+dnewline:
+        inc     CV              ; Bump to next line
+        lda     CV
+        cmp     WNDBTM          ; Are we at bottom?
+        bcc     :+
+        dec     CV              ; Yes, decrement
+        jsr     _scrollup_one   ; and scroll
+        lda     CV
+:       jmp     FVTABZ
 
 special_chars:
         cmp     #$0D            ; Test for \r = carrage return
@@ -98,26 +102,22 @@ _dputc:
         bcc     special_chars   ; Skip other tests if possible
 invert: eor     #$80            ; Invert high bit
 
-        .ifndef __APPLE2ENH__
+        bit     machinetype
+        bmi     dputdirect
         cmp     #$E0            ; Test for lowercase
         bcc     dputdirect
-        and     #$DF            ; Convert to uppercase
-        .endif
+        and     uppercasemask
 
 dputdirect:
-        .ifndef __APPLE2ENH__
         jsr     putchar
-        .else
-        jsr     putchardirect
-        .endif
 
-        .ifdef __APPLE2ENH__
+        bit     machinetype
+        bpl     inc40
         bit     RD80VID
         bpl     inc40
         inc     OURCH
         lda     OURCH
-        bra     cmpw
-        .endif
+        jmp     cmpw
 inc40:
         inc     CH              ; Bump to next column
         lda     CH
@@ -129,24 +129,24 @@ cmpw:
 :       jsr     dnewline
 left:
         lda     #$00
-        .ifdef  __APPLE2ENH__
+        bit     machinetype
+        bpl     zero40
         bit     RD80VID
         bpl     zero40
         sta     OURCH
-        bra     out
-        .endif
+        jmp     out
 zero40:
         sta     CH
 out:
         rts
 
 backspace:
-        .ifdef  __APPLE2ENH__
+        bit     machinetype
+        bpl     ld40
         bit     RD80VID
         bpl     ld40
         lda     OURCH
-        bra     chk
-        .endif
+        jmp     chk
 ld40:
         lda     CH              ; are we at col 0
 chk:
@@ -158,35 +158,25 @@ chk:
         sty     CV              ; store it
         ldy     WNDWDTH         ; prepare CH for decr
 
-        .ifdef  __APPLE2ENH__
+        bit     machinetype
+        bpl     stch
         bit     RD80VID
         bpl     stch
         sty     OURCH
-        bra     upcv
-        .endif
+        jmp     upcv
 stch:   sty     CH
 upcv:   lda     CV
         jsr     FVTABZ
 
 decrh:
-        .ifdef  __APPLE2ENH__
+        bit     machinetype
+        bpl     decch
         bit     RD80VID
         bpl     decch
         dec     OURCH
         rts
-        .endif
 decch:
         dec     CH
 bout:   rts
-
-dnewline:
-        inc     CV              ; Bump to next line
-        lda     CV
-        cmp     WNDBTM          ; Are we at bottom?
-        bcc     :+
-        dec     CV              ; Yes, decrement
-        jsr     _scrollup_one   ; and scroll
-        lda     CV
-:       jmp     FVTABZ
 
 .endif
