7a1859c3b7108fee4970ae834c9acd4a4f232168
Colin Leroy-Mira <colin@colino.net>
2025-09-13 16:50:08 +0200

    Allow overriding default quote policy in composer

--- src/mastodon/api/compose.h
+++ src/mastodon/api/compose.h
@@ -1,6 +1,7 @@
 #ifndef __compose_h
 #define __compose_h
 
+#include "account.h"
 #include "common.h"
 #include "poll.h"
 
@@ -13,10 +14,11 @@ extern const char *compose_poll_durations_seconds[];
 void compose_set_num_chars(void);
 
 char *compose_audience_str(char compose_audience);
-
+char *quote_policy_str(char quote_policy);
 signed char api_send_toot(char mode, char *buffer, char *cw, char sensitive_medias,
                           char *ref_toot_id, char **media_ids, char n_medias,
-                          poll *toot_poll, char compose_audience, char **err);
+                          poll *toot_poll, char compose_audience,
+                          char quote_policy, char **err);
 
 char *api_send_hgr_image(char *filename, char *description, char **err, char x, char y, char w);
 
