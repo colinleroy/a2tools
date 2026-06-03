d528cdbd9fc96777951eab3c42566e3b512cdede
Colin Leroy-Mira <colin@colino.net>
2026-06-03 20:26:25 +0200

    Release

--- src/surl-server/surl_protocol.h
+++ src/surl-server/surl_protocol.h
@@ -3,7 +3,7 @@
 
 /* Update in .inc too! */
 #define SURL_PROTOCOL_VERSION 25
-#define VERSION "25.0.1"
+#define VERSION "25.0.3"
 
 #define SURL_CLIENT_READY           0x2F
 #define HGR_LEN                     8192U
