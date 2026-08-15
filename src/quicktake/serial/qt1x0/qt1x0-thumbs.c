61cdc2ad7ca89c7bce0673f076b20d8da7d1503e
Colin Leroy-Mira <colin@colino.net>
2026-08-15 23:24:12 +0200

    Separate and optimize QT1x0 thumbnail loading

--- src/quicktake/serial/qt1x0/qt1x0-thumbs.c
+++ src/quicktake/serial/qt1x0/qt1x0-thumbs.c
@@ -8,27 +8,18 @@
 extern uint8 is_qt100;
 extern int ifd;
 
-#ifndef __CC65__
 uint8 *cur_thumb_data;
-#endif
 
-/* FIXME this should be in QT1X0 segment */
 #pragma code-name(push, "QT1X0")
-
 void qt1x0_thumb_histogram(void) {
   register signed char page;
   register unsigned char x;
   uint16 curr_hist = 0;
   uint16 read_len;
 
-#ifndef __CC65__
   bzero(histogram, sizeof(histogram));
-#else
-  bzero(err_buf, sizeof(err_buf));
-#endif
 
   /* First count values */
-#ifndef __CC65__
   uint8 r_bytes;
   while ((r_bytes = read(ifd, buffer, 255)) != 0) {
     cur_thumb_data = buffer;
@@ -39,59 +30,12 @@ void qt1x0_thumb_histogram(void) {
       cur_thumb_data++;
     } while (r_bytes--);
   }
-#else
-  read_len = 2048;
-  page = 8+1;
-  x = 0;
-read_again:
-  read(ifd, buffer, read_len);
-  __asm__("lda #>%v", buffer);
-  __asm__("sta %g+2", next_byte);
-  __asm__("ldy %v", x);
-next_page:
-    next_byte:
-    __asm__("lda %v,y", buffer); /* read byte - offset -1 to go to zero */
-    __asm__("sta tmp1"); /* backup it */
-    __asm__("asl"); /* << 4 low nibble */
-    __asm__("asl");
-    __asm__("asl");
-    __asm__("asl");
 
-    __asm__("tax");
-    __asm__("inc %v,x", err_buf);
-    __asm__("bne %g", noof22);
-    __asm__("inc %v+256,x", err_buf);
-    noof22:
-
-    __asm__("lda tmp1");
-    __asm__("and #$F0");
-    __asm__("tax");
-    __asm__("inc %v,x", err_buf);
-    __asm__("bne %g", noof23);
-    __asm__("inc %v+256,x", err_buf);
-    noof23:
-
-    __asm__("dey");
-    __asm__("bne %g", next_byte);
-    
-    __asm__("inc %g+2", next_byte);
-    __asm__("dec %v", page);
-    __asm__("bmi %g", done);
-    __asm__("bne %g", next_page);
-    
-    //last partial page
-    __asm__("ldy #%b", 2400-256*9);
-    __asm__("sty %v", x);
-    __asm__("bne %g", read_again);
-done:
-#endif
   /* Rewind for later load */
   lseek(ifd, 0, SEEK_SET);
 
   x = 0;
-
   /* Now equalize */
-#ifndef __CC65__
   do {
     uint32 tmp_large;
     uint16 tmp;
@@ -103,42 +47,17 @@ done:
     opt_histogram[x] = curr_hist/20;
     x += 0x10;
   } while (x);
-#else
-    // curr_hist += histogram_low[x]|(histogram_high[x]<<8);
-    __asm__("lda #$00");
-next:
-    __asm__("sta %v", x);
-    __asm__("tax");
-    __asm__("clc");
-    __asm__("lda %v,x", err_buf);
-    __asm__("adc %v", curr_hist);
-    __asm__("sta %v", curr_hist);
-    __asm__("lda %v+256,x", err_buf);
-    __asm__("adc %v+1", curr_hist);
-    __asm__("sta %v+1", curr_hist);
-    __A__ = curr_hist / 20;
-    // opt_histogram[x] = tmp;
-    __asm__("ldx %v", x);
-    __asm__("sta %v,x", opt_histogram);
-    __asm__("txa");
-    __asm__("clc");
-    __asm__("adc #$10");
-    __asm__("bne %g", next);
-#endif
 }
 
 /* FIXME this should be in QT1X0 segment */
 #pragma code-name(pop)
 void qt1x0_load_thumb_data(uint8 line) {
-#ifndef __CC65__
   uint8 a, b, c, d, dx, i;
-#endif
   /* assume thumbnail at 4bpp and zoom it */
   if (is_qt100) {
     if (!(line & 1)) {
       read(ifd, THUMBNAIL_BUF_START, THUMB_WIDTH / 2);
       /* Unpack */
-#ifndef __CC65__
       uint8 off;
       i = 39;
       do {
@@ -151,42 +70,13 @@ void qt1x0_load_thumb_data(uint8 line) {
         THUMBNAIL_BUF_START[off++] = b;
         THUMBNAIL_BUF_START[off] = b;
       } while (i--);
-#else
-      __asm__("ldy #39");
-      __asm__("ldx #156");
-
-next_thumb_x:
-      __asm__("lda %v+%b,y", buffer, THUMBNAIL_BUFFER_OFFSET); /* Load byte at index Y */
-      __asm__("sta tmp2");  /* backup value */
-      __asm__("asl");       /* low nibble, << 4 */
-      __asm__("asl");
-      __asm__("asl");
-      __asm__("asl");
-      __asm__("sta %v+%b,x", buffer, THUMBNAIL_BUFFER_OFFSET);
-      __asm__("dex");
-      __asm__("sta %v+%b,x", buffer, THUMBNAIL_BUFFER_OFFSET);
-
-      __asm__("lda tmp2");  /* restore value */
-      __asm__("and #$F0"); /* high nibble */
-      __asm__("dex");
-      __asm__("sta %v+%b,x", buffer, THUMBNAIL_BUFFER_OFFSET);
-      __asm__("dex");
-      __asm__("sta %v+%b,x", buffer, THUMBNAIL_BUFFER_OFFSET);
-      __asm__("dex");
-
-      __asm__("dey");
-      __asm__("bpl %g", next_thumb_x);
-#endif
     }
   } else { /* QT150 */
-#ifndef __CC65__
     unsigned char *cur_in, *cur_out;
-#endif
     /* Whyyyyyy do they do that */
     if (!(line % 4)) {
       /* Expand the next two lines from 4bpp thumb_buf to 8bpp buffer */
       read(ifd, thumb_buf, THUMB_WIDTH);
-#ifndef __CC65__
       cur_in = thumb_buf;
       cur_out = buffer+THUMBNAIL_BUFFER_OFFSET;
       for (dx = 0; dx < THUMB_WIDTH; dx++) {
@@ -196,31 +86,8 @@ next_thumb_x:
         b   = ((c & 0x0F) << 4);
         *cur_out++ = b;
       }
-#else
-      __asm__("ldx #0");
-      __asm__("ldy #0");
-next_expand_x:
-      __asm__("lda %v,x", thumb_buf);
-      __asm__("sta tmp1");
-      __asm__("and #$F0"); /* high nibble */
-      __asm__("sta %v+%b,y", buffer, THUMBNAIL_BUFFER_OFFSET);
-      __asm__("iny");
-
-      __asm__("lda tmp1");  /* low nibble, << 4 */
-      __asm__("asl");
-      __asm__("asl");
-      __asm__("asl");
-      __asm__("asl");
-      __asm__("sta %v+%b,y", buffer, THUMBNAIL_BUFFER_OFFSET);
-      __asm__("iny");
-
-      __asm__("inx");
-      __asm__("cpx #%b", THUMB_WIDTH);
-      __asm__("bcc %g", next_expand_x);
-#endif
 
       /* Reorder bytes from buffer back to thumb_buf */
-#ifndef __CC65__
       cur_in = buffer+THUMBNAIL_BUFFER_OFFSET;
       cur_out = thumb_buf;
       for (i = 0; i < THUMB_WIDTH*3/2; ) {
@@ -249,49 +116,10 @@ next_expand_x:
 
         i++;
       }
-#else
-      __asm__("lda #>%v", thumb_buf);
-      __asm__("sta %g+2", out_high_page);
 
-      __asm__("ldx #0");  /* in */
-      __asm__("ldy #0");  /* out */
-next_three_quarters_x:
-      __asm__("lda %v+%b,x", buffer, THUMBNAIL_BUFFER_OFFSET);
-      __asm__("sta %v,y", thumb_buf);
-
-      __asm__("inx");
-      __asm__("iny");
-      __asm__("lda %v+%b,x", buffer, THUMBNAIL_BUFFER_OFFSET);
-      __asm__("sta %v,y", thumb_buf);
-
-      __asm__("inx");
-      __asm__("lda %v+%b,x", buffer, THUMBNAIL_BUFFER_OFFSET);
-      __asm__("sta %v+%b,y", thumb_buf, THUMB_WIDTH-1);
-
-      __asm__("iny");
-      __asm__("inx");
-
-      __asm__("cpx #%b", THUMB_WIDTH*3/2);
-      __asm__("bcc %g", next_three_quarters_x);
-
-last_quarter_x:
-      __asm__("iny");
-      __asm__("lda %v+%b,x", buffer, THUMBNAIL_BUFFER_OFFSET);
-out_high_page:
-      __asm__("sta %v,y", thumb_buf);
-      __asm__("iny");
-      __asm__("inx");
-      __asm__("bne %g", check_bound);
-      __asm__("inc %g+2", out_high_page);
-check_bound:
-      __asm__("cpx #<%w", (THUMB_WIDTH*2));
-      __asm__("bcc %g", last_quarter_x);
-
-#endif
-
+qt150_second_line:
       /* Finally copy the first line of thumb_buf to buffer for display,
        * upscaling horizontally */
-#ifndef __CC65__
       cur_in = thumb_buf;
       cur_out = THUMBNAIL_BUF_START;
       for (dx = 0; dx < THUMB_WIDTH; dx++) {
@@ -301,46 +129,8 @@ check_bound:
         cur_out++;
         cur_in++;
       }
-#else
-      __asm__("ldx #0");
-      __asm__("ldy #0");
-next_copy_x:
-      __asm__("lda %v,x", thumb_buf);
-      __asm__("sta %v+%b,y", buffer, THUMBNAIL_BUFFER_OFFSET);
-      __asm__("iny");
-      __asm__("sta %v+%b,y", buffer, THUMBNAIL_BUFFER_OFFSET);
-      __asm__("iny");
-      __asm__("inx");
-      __asm__("cpx #%b", THUMB_WIDTH);
-      __asm__("bcc %g", next_copy_x);
-#endif
-
     } else if (!(line % 2)) {
-      /* Copy the second line of thumb_buf to buffer for display,
-       * upscaling horizontally */
-#ifndef __CC65__
-      cur_in = thumb_buf + THUMB_WIDTH;
-      cur_out = THUMBNAIL_BUF_START;
-      for (dx = 0; dx < THUMB_WIDTH; dx++) {
-        *cur_out = *cur_in;
-        cur_out++;
-        *cur_out = *cur_in;
-        cur_out++;
-        cur_in++;
-      }
-#else
-      __asm__("ldx #0");
-      __asm__("ldy #0");
-next_copy_x2:
-      __asm__("lda %v+%b,x", thumb_buf, THUMB_WIDTH);
-      __asm__("sta %v+%b,y", buffer, THUMBNAIL_BUFFER_OFFSET);
-      __asm__("iny");
-      __asm__("sta %v+%b,y", buffer, THUMBNAIL_BUFFER_OFFSET);
-      __asm__("iny");
-      __asm__("inx");
-      __asm__("cpx #%b", THUMB_WIDTH);
-      __asm__("bcc %g", next_copy_x2);
-#endif
+      goto qt150_second_line;
     } else {
       /* Reuse the previous buffer line once for upscaling */
     }
---
+++ src/quicktake/serial/qt1x0/qt1x0-thumbs.s
@@ -0,0 +1,255 @@
+        .export           _qt1x0_thumb_histogram
+        .export           _qt1x0_load_thumb_data
+
+        .import           _is_qt100, _err_buf, _thumb_buf
+
+        .import           _read, _lseek, _ifd, _buffer, _opt_histogram
+        .import           pushax, pusha0, push0ax, tossub0ax
+        .import           tosudiva0
+
+        .importzp         _zp6, _zp8, _zp9, _zp10, tmp1, tmp2, sreg
+
+        .include          "../qt-thumbs.inc"
+        .include          "stdio.inc"
+        .include          "fcntl.inc"
+
+; _qt1x0_thumb_histogram
+page            = _zp6        ; word
+rem_bytes       = _zp8        ; byte
+cur_byte        = _zp9        ; byte
+curr_hist       = _zp10       ; word
+
+; Fixme this should be in QT1X0 segment but there's not enough room
+.segment "CODE"
+
+.proc _qt1x0_thumb_histogram
+        lda     #0            ; Init values
+        sta     rem_bytes     ; 256 bytes
+        lda     #(8+1)        ; 8 pages
+        sta     page
+read_data:
+        lda     _ifd          ; Read from thumbnail file
+        jsr     pusha0
+
+        lda     #<_buffer
+        ldx     #>_buffer
+        jsr     pushax
+        lda     #<2048
+        ldx     #>2048
+        jsr     _read
+
+        lda     #>_buffer     ; Set walker to buffer start
+        sta     next_byte+2
+        ldy     rem_bytes     ; How many bytes to read?
+next_page:
+next_byte:
+        lda     _buffer,y     ; Read a byte (2 pixels)
+        sta     tmp1
+        asl                   ; First pixel
+        asl
+        asl
+        asl
+        tax
+        inc     _err_buf,x
+        bne     :+
+        inc     _err_buf+256,x
+
+:       lda     tmp1          ; Second pixel
+        and     #$F0
+        tax
+        inc     _err_buf,x    ; histogram[x]++
+        bne     :+
+        inc     _err_buf+256,x
+
+:       dey                   ; Anymore in this page ?
+        bne     next_byte
+        inc     next_byte+2
+        dec     page          ; Any more pages ?
+        bmi     done          ; if negative, we did the partial page
+        bne     next_page     ; if not zero, more full pages
+
+        ldy     #<(THUMBNAIL_SIZE-256-9) ; Last partial page
+        sty     rem_bytes
+        bne     read_data
+
+done:
+        ; We have counted values. Now equalize
+        lda     _ifd          ; Rewind file
+        jsr     pusha0
+
+        lda     #0
+        tax
+        jsr     push0ax
+        lda     #<SEEK_SET
+        ldx     #>SEEK_SET
+        jsr     _lseek
+
+        lda     #$00
+next_val:
+        sta     cur_byte
+        tax
+        clc
+        lda     _err_buf,x    ; curr_hist += histogram[x]
+        adc     curr_hist
+        sta     curr_hist
+        tay
+        lda     _err_buf+256,x
+        adc     curr_hist+1
+        sta     curr_hist+1
+
+        tax                   ; curr_hist/20 (equ to *$F0 / 80 / 60)
+        tya
+        jsr     pushax
+        lda     #20
+        jsr     tosudiva0
+
+        ldx     cur_byte      ; opt_histogram[x] = curr_hist/20
+        sta     _opt_histogram,x
+
+        txa                   ; x += $10 (as we only have $x0 values in thumbnails)
+        clc
+        adc     #$10
+        bne     next_val
+        rts
+.endproc
+
+.segment "QT1X0"
+
+.proc _qt1x0_load_thumb_data
+        ldy     _is_qt100
+        beq     load_qt150_line
+load_qt100_line:
+        and     #$01          ; line & 1 ? if odd, return, to double lines
+        beq     :+
+        rts
+
+:       lda     _ifd          ; Read from thumbnail file
+        jsr     pusha0
+
+        lda     #<(_buffer+THUMBNAIL_BUFFER_OFFSET)
+        ldx     #>(_buffer+THUMBNAIL_BUFFER_OFFSET)
+        jsr     pushax
+        lda     #<(THUMB_WIDTH/2)
+        ldx     #>(THUMB_WIDTH/2)
+        jsr     _read
+
+        ldy     #((THUMB_WIDTH/2)-1)
+        ldx     #((THUMB_WIDTH/2)-1)*4
+
+next_thumb_x:
+        lda     _buffer+THUMBNAIL_BUFFER_OFFSET,y
+        sta     tmp2          ; backup value for second pixel
+        asl
+        asl
+        asl
+        asl
+        sta     _buffer+THUMBNAIL_BUFFER_OFFSET,x
+        dex                   ; Store twice to double width
+        sta     _buffer+THUMBNAIL_BUFFER_OFFSET,x
+
+        lda     tmp2          ; Second pixel
+        and     #$F0
+        dex
+        sta     _buffer+THUMBNAIL_BUFFER_OFFSET,x
+        dex                   ; Store twice to double width
+        sta     _buffer+THUMBNAIL_BUFFER_OFFSET,x
+        dex
+
+        dey
+        bpl     next_thumb_x
+        rts
+
+load_qt150_line:
+        and     #$03          ; Load two lines at once, and bump them to 4
+        beq     qt150_first_line
+        and     #$01
+        beq     qt150_second_line
+        rts
+
+qt150_first_line:
+        lda     _ifd          ; Read from thumbnail file
+        jsr     pusha0
+
+        lda     #<_thumb_buf
+        ldx     #>_thumb_buf
+        jsr     pushax
+        lda     #<THUMB_WIDTH
+        ldx     #>THUMB_WIDTH
+        jsr     _read
+
+        ldx     #$00
+        ldy     #$00
+
+next_expand_x:                ; expand nibbles to bytes
+        lda     _thumb_buf,x
+        sta     tmp1
+        and     #$F0          ; high nibble
+        sta     _buffer+THUMBNAIL_BUFFER_OFFSET,y
+        iny
+
+        lda     tmp1          ; low nibble
+        asl
+        asl
+        asl
+        asl
+        sta     _buffer+THUMBNAIL_BUFFER_OFFSET,y
+        iny
+
+        inx
+        cpx     #THUMB_WIDTH
+        bcc     next_expand_x
+
+        ; Reorder bytes across two rows: x/y, x+1/y,x/y+1 for 120 bytes, then x+1,y+1 for 40 bytes
+        lda     #>_thumb_buf
+        sta     out_high
+
+        ldx     #$00          ; in pointer
+        ldy     #$00          ; out pointer
+
+reorder_120:
+        lda     _buffer+THUMBNAIL_BUFFER_OFFSET,x ; X/Y
+        sta     _thumb_buf,y
+
+        inx
+        iny
+        lda     _buffer+THUMBNAIL_BUFFER_OFFSET,x ; X+1/Y
+        sta     _thumb_buf,y
+
+        inx
+        lda     _buffer+THUMBNAIL_BUFFER_OFFSET,x ; X/Y+1
+        sta     _thumb_buf+THUMB_WIDTH-1,y
+
+        iny
+        inx
+        cpx     #(THUMB_WIDTH*3/2)
+        bcc     reorder_120
+
+reorder_40:
+        iny
+        lda     _buffer+THUMBNAIL_BUFFER_OFFSET,x ; X+1/Y
+out_high:
+        sta     _thumb_buf,y
+        iny
+        inx
+        bne     check_reorder_bound
+        inc     out_high                          ; Last pixels are on page 2
+check_reorder_bound:
+        cpx     #<(THUMB_WIDTH*2)
+        bcc     reorder_40
+
+        ; Done! finally, copy first line to second line for upscaling
+qt150_second_line:
+        ldx     #$00
+        ldy     #$00
+next_copy_x:
+        lda     _thumb_buf,x
+        sta     _buffer+THUMBNAIL_BUFFER_OFFSET,y
+        iny
+        sta     _buffer+THUMBNAIL_BUFFER_OFFSET,y
+        iny
+        inx
+        cpx      #THUMB_WIDTH
+        bcc      next_copy_x
+        rts
+
+.endproc
