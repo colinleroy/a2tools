167531562586db8fe060d60dea5f70a96e9376d0
Colin Leroy-Mira <colin@colino.net>
2026-08-22 19:12:57 +0200

    Serial: Slow down for timeout on accelerated machines

--- src/lib/serial/asm/simple_serial_read_no_irq.s
+++ src/lib/serial/asm/simple_serial_read_no_irq.s
@@ -9,31 +9,45 @@ bytes      = tmp1
 full_pages = tmp2
 timeout    = ptr2
 
+; Returns with Z set on timeout
+.proc check_timeout
+        inc     timeout
+        bne     out
+        inc     timeout+1
+out:    rts
+.endproc
+
+.proc reset_timeout
+        lda     #$00
+        sta     timeout
+        sta     timeout+1
+        rts
+.endproc
+
 ; int simple_serial_read_no_irq(char *buffer, size_t len)
 .proc _simple_serial_read_no_irq
         sta     bytes
         stx     full_pages
         jsr     popptr1                 ; Doesn't touch X
 
-        ldy     #$00
-        sty     timeout
-        sty     timeout+1
+        jsr     reset_timeout
+        tay                             ; A = 0 here
 
         cpx     #$00
         beq     finish_last_bytes
 
 get_bytes:
+        bit     $C070                   ; Slow accelerators
         jsr     _serial_read_byte_direct
         bcc     store_byte_page
 
-        inc     timeout                 ; Read no byte, inc timeout
-        bne     get_bytes
-        inc     timeout+1
+        jsr     check_timeout
         bne     get_bytes
         beq     timed_out
 
 store_byte_page:                        ; Store byte,
         sta     (ptr1),y
+        jsr     reset_timeout
         iny                             ; increment output,
         bne     get_bytes
         inc     ptr1+1                  ; increment output page,
@@ -46,17 +60,17 @@ finish_last_bytes:
         ldy     #$00
 
 get_last_bytes:
+        bit     $C070                   ; Slow accelerators
         jsr     _serial_read_byte_direct
         bcc     store_last_byte
 
-        inc     timeout                 ; Read no byte, inc timeout
-        bne     get_last_bytes
-        inc     timeout+1
+        jsr     check_timeout
         bne     get_last_bytes
         beq     timed_out
 
 store_last_byte:                        ; Store byte,
         sta     (ptr1),y
+        jsr     reset_timeout
         iny                             ; increment output,
         dex                             ; decrement byte count
         bne     get_last_bytes
